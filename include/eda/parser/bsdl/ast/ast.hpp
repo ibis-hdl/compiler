0235bcf45b5c1f3d71e5eb3bec8c2b48b55506ba
Olaf <olaf@home.lan>
2017-02-25 21:06:28 +0100

    added, but empty

---
+++ include/eda/parser/bsdl/ast/ast.hpp
@@ -0,0 +1,15 @@
+/*
+ * ast.hpp
+ *
+ *  Created on: 25.02.2017
+ *      Author: olaf
+ */
+
+#ifndef INCLUDE_EDA_PARSER_BSDL_AST_AST_HPP_
+#define INCLUDE_EDA_PARSER_BSDL_AST_AST_HPP_
+
+
+
+
+
+#endif /* INCLUDE_EDA_PARSER_BSDL_AST_AST_HPP_ */
