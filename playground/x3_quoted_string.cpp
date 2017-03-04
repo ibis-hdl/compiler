0da8862d53f4e481d092a30d48cd6e61c26e2b49
Olaf <olaf@home.lan>
2017-03-04 21:12:32 +0100

    added

--- playground/x3_quoted_string.cpp
+++ playground/x3_quoted_string.cpp
@@ -8,10 +8,19 @@
 
 #include <boost/spirit/home/x3.hpp>
 #include <iostream>
+#include <vector>
 
 namespace parser {
 
 	namespace x3 = boost::spirit::x3;
+	namespace iso8859_1 = boost::spirit::x3::iso8859_1;
+
+	using iso8859_1::upper;
+	using iso8859_1::lower;
+	using iso8859_1::digit;
+	using iso8859_1::punct;
+	using iso8859_1::space;
+	using iso8859_1::graph;
 
 	struct basic_graphic_character_class;
 	struct graphic_character_class;
@@ -26,21 +35,21 @@ namespace parser {
 	string_literal_type const string_literal { "string_literal" };
 
 	auto const basic_graphic_character_def =
-		  x3::upper
-		| x3::digit
-		| x3::punct // special_character
-		| x3::space
+		  upper
+		| digit
+		| punct // special_character
+		| space
 		;
 
 	auto const graphic_character_def =
 		  basic_graphic_character
-		| x3::lower
-		| x3::graph // other_special_character
+		| lower
+		| graph // other_special_character
 		;
 
 	auto const string_literal_def =
 		x3::lexeme[
-			'"' >> ( +(graphic_character - '"') ) >> '"'
+			'"' >> ( *(graphic_character - '"') ) >> '"'
 		]
 		;
 
@@ -55,23 +64,43 @@ int main()
 {
 	namespace x3 = boost::spirit::x3;
 
-	std::string const input { "\" aa\"" };
+	std::vector<std::string> const test_cases {
+		"\" aa\"",
+		"\"aa \"",
+		"\" aa \"",
+		"\".\"",
+		"\" \"",
+		"\"ä\"",
+		"\"\""
+		};
 
     typedef std::string::const_iterator iterator_type;
 
-    iterator_type iter = input.begin();
-    iterator_type const end = input.end();
+    for(auto str: test_cases) {
+		iterator_type iter = str.begin();
+		iterator_type const end = str.end();
 
-    auto& rule = parser::string_literal;
+		auto& rule = parser::string_literal;
 
-    bool r = phrase_parse(iter, end, rule, x3::space);
+		bool r = phrase_parse(iter, end, rule, x3::space);
 
-    if (r && iter == end) {
-    	std::cout << "Parsing succeeded\n";
-    } else {
-    	std::cout << "Parsing failed\n";
+		if (r && iter == end) {
+			std::cout << "Parsing succeeded\n";
+		} else {
+			std::cout << "Parsing failed\n";
+		}
     }
 
     return 0;
 }
 
+/*
+LANG=en g++ -std=c++14 ../IBIS_SOURCE/playground/x3_quoted_string.cpp -o x3_quoted_string && ./x3_quoted_string
+Parsing succeeded
+Parsing succeeded
+Parsing succeeded
+Parsing succeeded
+Parsing succeeded
+Parsing succeeded
+Parsing failed
+ */
