4c0da7c4997222b4402e049cffceedeb90f7fd92
Olaf <olaf@home.lan>
2017-03-07 20:10:33 +0100

    update

--- cmake/external-gsl.cmake
+++ cmake/external-gsl.cmake
@@ -1,14 +1,28 @@
 message("External project: C++ Guideline Support Library (Microsoft/GSL)")
 
+
+
 ExternalProject_Add(
-    gsl
-    GIT_REPOSITORY        https://github.com/Microsoft/GSL.git
-    GIT_TAG               master
-    PREFIX                ${PROJECT_BINARY_DIR}/external
-    CONFIGURE_COMMAND     ""
-    BUILD_COMMAND         ""
-    INSTALL_COMMAND       ""
+  gsl
+  GIT_REPOSITORY        https://github.com/Microsoft/GSL.git
+  GIT_TAG               master
+  PREFIX                ${PROJECT_BINARY_DIR}/external
+  PATCH_COMMAND         ""
+  CONFIGURE_COMMAND     ""
+  BUILD_COMMAND         ""
+  INSTALL_COMMAND       ""
 )
 
 ExternalProject_Get_Property(gsl source_dir)
 set(GSL_INCLUDE_DIR ${source_dir}/include)
+
+
+# create a .no_backup file for bacula
+ExternalProject_Get_Property(gsl PREFIX)
+
+ExternalProject_Add_Step(
+  gsl
+  tag_no_backup
+  COMMAND              touch ${PREFIX}/.no_backup
+) 
+
--- playground/vhdl93/x3_integer.cpp
+++ playground/vhdl93/x3_integer.cpp
@@ -5,76 +5,90 @@
  *      Author: olaf
  */
 
+
 #include <iosfwd>
 //#define BOOST_SPIRIT_X3_DEBUG
 
 #include <boost/spirit/home/x3.hpp>
+
 #include <iostream>
+#include <vector>
+#include <algorithm> // for copy
+#include <iterator> // for ostream_iterator
+#include <typeinfo>
 
 namespace parser {
 
-    namespace x3 = boost::spirit::x3;
-    namespace iso8859_1 = boost::spirit::x3::iso8859_1;
+  namespace x3 = boost::spirit::x3;
+  namespace iso8859_1 = boost::spirit::x3::iso8859_1;
 
-    using iso8859_1::char_;
+  using iso8859_1::char_;
 
-    struct integer_class;
+  typedef x3::rule<struct integer_class, std::vector<int>> integer_type;
 
-    typedef x3::rule<integer_class, int> integer_type;
+  integer_type const integer { "integer" };
 
-    integer_type const integer { "integer" };
 
-    // Hint: uint_parser<unsigned, 10, 3, 3> uint3_3_p;      //  exactly 3 digits
-    auto const integer_def =
+  auto combine_to_int = [](auto &ctx) {
+    using x3::_val;
+    auto& v = _val(ctx);
+    std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, ", "));
+  };
+
+  // Parse '1_000' as 1000
+  // Note: 'x3::digit >> *( -char_('_') >> x3::digit )' parses also '_' into
 #if 1
-        x3::omit[
-             x3::digit >> *( -char_('_') >> x3::digit )
-        ]
+  auto const integer_def =
+    (
+     x3::digit >> *( x3::omit[ -char_('_') ] >> x3::digit )
+    )
 #else
-        x3::int_
+  auto const integer_def %=
+    (
+     x3::digit >> *( x3::omit[ -char_('_') ] >> x3::digit )
+    )[combine_to_int]
 #endif
-        ;
+    ;
 
-    BOOST_SPIRIT_DEFINE(
-        integer
-    );
+  BOOST_SPIRIT_DEFINE(integer)
 }
 
 
 int main()
 {
-    namespace x3 = boost::spirit::x3;
+  namespace x3 = boost::spirit::x3;
 
-    std::vector<std::string> const test_cases {
-        "1",
-        "42_666_4711"
-        };
+  std::vector<std::string> const test_cases {
+      "0",
+      "1",
+      "01",
+      "1_000",
+      "42_666_4711"
+      };
 
-    typedef std::string::const_iterator iterator_type;
+  typedef std::string::const_iterator iterator_type;
 
-    for(auto str: test_cases) {
-        iterator_type iter = str.begin();
-        iterator_type const end = str.end();
+  for(auto str: test_cases) {
+    iterator_type iter = str.begin();
+    iterator_type const end = str.end();
 
-        int result;
-        auto& rule = parser::integer;
+    auto& rule = parser::integer;
 
-        std::cout << "parse " << str << ": ";
+    std::cout << "parse " << str << ": ";
 
-        bool r = x3::phrase_parse(iter, end, rule, rule /*, result*/);
+    std::vector<int> i;
 
-        if (r && iter == end) {
-            std::cout << "succeeded\n";
-        } else {
-            std::cout << "failed\n";
-        }
+    bool r = x3::phrase_parse(iter, end, rule, x3::space, i);
+
+    if (r && iter == end) {
+      std::cout << "succeeded\n";
+      std::copy(i.begin(), i.end(), std::ostream_iterator<char>(std::cout, " "));
+      std::cout << '\n';
+    } else {
+      std::cout << "failed\n";
     }
+  }
 
-    return 0;
+  return 0;
 }
-
-
-
-
-
 
