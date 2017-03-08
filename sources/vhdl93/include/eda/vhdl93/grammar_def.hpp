7e69f76f3f9552bbcb199712e62ad44ce6538aa3
Olaf <olaf@home.lan>
2017-03-08 16:01:31 +0100

    skipper added

--- sources/vhdl93/include/eda/vhdl93/grammar_def.hpp
+++ sources/vhdl93/include/eda/vhdl93/grammar_def.hpp
@@ -3662,4 +3662,34 @@ BOOST_SPIRIT_DEFINE(
 } } } // namespace eda.vhdl93.parser
 
 
+/*
+ * VHDL Skipper
+ */
+namespace eda { namespace vhdl93 { namespace parser {
+
+// VHDL char set is iso8859:1
+
+namespace x3 = boost::spirit::x3;
+namespace iso8859_1 = boost::spirit::x3::iso8859_1;
+
+using iso8859_1::char_;
+using iso8859_1::space;
+
+
+struct skipper_class;
+typedef x3::rule<skipper_class> skipper_type;
+skipper_type const skipper { "skipper" };
+
+auto const skipper_def =
+      space
+    | "--" >> (*char_ - x3::eol) >> x3::eol
+    ;
+
+BOOST_SPIRIT_DEFINE(skipper)
+
+
+} } } // namespace eda.vhdl93.parser
+
+
+
 #endif /* SOURCES_VHDL93_INCLUDE_EDA_VHDL93_GRAMMAR_DEF_HPP_ */
