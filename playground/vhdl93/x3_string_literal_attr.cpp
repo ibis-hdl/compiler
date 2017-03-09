e79633e51eb154ea238ed433f94a97328e49feeb
Olaf <olaf@home.lan>
2017-03-09 16:20:42 +0100

    fixed

--- playground/vhdl93/x3_string_literal_attr.cpp
+++ playground/vhdl93/x3_string_literal_attr.cpp
@@ -1,113 +1,161 @@
 /*
- * x3_quoted_string.cpp
+ * x3_string_literal.cpp
  *
  *  Created on: 04.03.2017
  *      Author: olaf
  */
 
 
+#include <iosfwd>
+//#define BOOST_SPIRIT_X3_DEBUG
+
 #include <boost/spirit/home/x3.hpp>
-
-#include <boost/fusion/include/adapt_struct.hpp>
-#include <boost/fusion/include/io.hpp>
-
 #include <iostream>
 #include <vector>
 
-// c++14 experimental
-#include <experimental/string_view>
-
-namespace boost { namespace spirit { namespace x3 { namespace traits {
-
-template <typename It>
-void move_to(It b, It e, std::experimental::string_view& v)
-{
-	// FixMe: storage is contiguous as a concept check for input range
-    v = std::experimental::string_view(&*b, std::size_t(std::distance(b,e)));
-}
-
-} } } }
-
-namespace ast
-{
-	using std_string_view = std::experimental::string_view;
-
-    struct foo
-    {
-        std_string_view string;
-    };
-
-    using boost::fusion::operator<<;
-}
-
-BOOST_FUSION_ADAPT_STRUCT(ast::foo,
-    string
-)
-
-
+/*
+ * http://rti.etf.bg.ac.rs/rti/ri5rvl/tutorial/TUTORIAL/IEEE/HTML/1076_13.HTM#13.6
+ */
 namespace parser {
 
 	namespace x3 = boost::spirit::x3;
 	namespace iso8859_1 = boost::spirit::x3::iso8859_1;
 
-	using iso8859_1::upper;
-	using iso8859_1::lower;
-	using iso8859_1::digit;
-	using iso8859_1::punct;
-	using iso8859_1::space;
-	using iso8859_1::graph;
+	using iso8859_1::char_;
 
 	struct basic_graphic_character_class;
 	struct graphic_character_class;
 	struct integer_class;
 
-	typedef x3::rule<basic_graphic_character_class> basic_graphic_character_type;
-	typedef x3::rule<graphic_character_class> graphic_character_type;
-	typedef x3::rule<integer_class, ast::foo> string_literal_type;
+	typedef x3::rule<basic_graphic_character_class, char> basic_graphic_character_type;
+	typedef x3::rule<graphic_character_class, char> graphic_character_type;
+	typedef x3::rule<integer_class, std::string> string_literal_type;
 
 	basic_graphic_character_type const basic_graphic_character { "basic_graphic_character" };
 	graphic_character_type const graphic_character { "graphic_character" };
 	string_literal_type const string_literal { "string_literal" };
 
+	// helper
+	struct upper_case_letter_class;
+	struct digit_class;
+	struct special_character_class;
+	struct space_character_class;
+	struct format_effector_class;
+	struct end_of_line_class;
+	struct lower_case_letter_class;
+	struct other_special_character_class;
+
+	typedef x3::rule<upper_case_letter_class, char> upper_case_letter_type;
+	typedef x3::rule<digit_class, char> digit_type;
+	typedef x3::rule<special_character_class, char> special_character_type;
+	typedef x3::rule<space_character_class, char> space_character_type;
+	typedef x3::rule<format_effector_class> format_effector_type;
+	typedef x3::rule<end_of_line_class> end_of_line_type;
+	typedef x3::rule<lower_case_letter_class, char> lower_case_letter_type;
+	typedef x3::rule<other_special_character_class, char> other_special_character_type;
+
+	upper_case_letter_type const upper_case_letter { "upper_case_letter" };
+	digit_type const digit { "digit" };
+	special_character_type const special_character { "special_character" };
+	space_character_type const space_character { "space_character" };
+	format_effector_type const format_effector { "format_effector" };
+	end_of_line_type const end_of_line { "end_of_line" };
+	lower_case_letter_type const lower_case_letter { "lower_case_letter" };
+	other_special_character_type const other_special_character { "other_special_character" };
+
+	auto const upper_case_letter_def = char_("ABCDEFGHIJKLMNOPQRSTUVWXYZÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞ");
+	auto const lower_case_letter_def = char_("abcdefghijklmnopqrstuvwxyzßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿ");
+	auto const digit_def = char_("0-9");
+	auto const special_character_def = char_("\"#&'()*+,-./:;<=>[]_|");
+	auto const other_special_character_def = char_("!$%?@\\^`{}~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿×÷");
+
+	auto const space_character_def = iso8859_1::space;
+	auto const format_effector_def = char_("\t\n\v\r\f");
+	auto const end_of_line_def = x3::eol;
+
+	BOOST_SPIRIT_DEFINE(
+		upper_case_letter,
+		digit,
+		special_character,
+		space_character,
+		format_effector,
+		end_of_line,
+		lower_case_letter,
+		other_special_character
+	)
+
 	auto const basic_graphic_character_def =
-		  upper
+		  upper_case_letter
 		| digit
-		| punct // special_character
-		| space
+		| special_character
+		| space_character
 		;
 
 	auto const graphic_character_def =
 		  basic_graphic_character
-		| lower
-		| graph // other_special_character
+		| lower_case_letter
+		| other_special_character
 		;
 
+	/* From Lexer (Thomas Dettmer) VHDL Archive:
+	 *
+	 * (\"({graphic_character}|(\"\")|\%)*\")|(\%({graphic_character}|(\%\%)|\")*\%) {
+	 *		ECHO; return ( t_StringLit );
+	 *
+	 *   ( " ( {graphic_character}
+     *       | ("")
+     *       | %
+     *       )*
+     *     "
+     *   )
+     * | ( % ( {graphic_character}
+     *       | (%%)
+     *       | "
+     *       )*
+     *     %
+     *   )
+	 *
+	 * Note, other_special_character doesn't contain '%' !
+	 */
+    // string_literal ::=  " { graphic_character } "
 	auto const string_literal_def =
-		x3::lexeme[ x3::raw[
-			'"' >> ( *(graphic_character - '"') ) >> '"'
-		] ]
+        x3::lexeme[
+            (      '"'
+                >> *( (graphic_character - '"')
+                    | x3::no_skip[char_('"') >> char_('"')]
+                    )
+                >> '"'
+            )
+            |
+            (      '%' // LRM Chapter 13.10
+                >> *( (graphic_character - '%')
+                    | x3::no_skip[char_('%') >> char_('%')]
+                    )
+                >> '%'
+            )
+        ]
 		;
 
 	BOOST_SPIRIT_DEFINE(
 		basic_graphic_character,
 		graphic_character,
 		string_literal
-	);
+	)
 }
 
 int main()
 {
 	namespace x3 = boost::spirit::x3;
-	namespace iso8859_1 = boost::spirit::x3::iso8859_1;
 
 	std::vector<std::string> const test_cases {
-		"\" aa\"",
-		"\"aa \"",
-		"\" aa \"",
-		"\".\"",
-		"\" \"",
-		"\"ä\"",
-		"\"\""
+		"\"Both S and Q equal to 1\"",
+		"\"Characters such as $, %, and } are allowed in string literals.\"",
+		"\"& ' ( ) * + , - . / : ; < = > | [ ]\"",
+		"\"Quotation: \"\"REPORT...\"\" is also allowed\"",
+		"%see \"LRM 13.10\", it's legal VHDL%",
+		"%Quotation: %%REPORT...%% is also allowed%",
+		// has to fail
+		"\"Both S and Q equal to 1",
 		};
 
     typedef std::string::const_iterator iterator_type;
@@ -116,16 +164,18 @@ int main()
 		iterator_type iter = str.begin();
 		iterator_type const end = str.end();
 
+		//auto& rule = parser::graphic_character;
 		auto& rule = parser::string_literal;
-		ast::foo foo;
+        std::string s;
+        
+		std::cout << "parse [" << str << "]: ";
 
-		bool r = phrase_parse(iter, end, rule, iso8859_1::space, foo);
+		bool r = phrase_parse(iter, end, rule, x3::space, s);
 
 		if (r && iter == end) {
-			std::cout << "Parsing succeeded, got "
-			          << foo.string << '\n';
+			std::cout << "Parsing succeeded: " << s << "\n";
 		} else {
-			std::cout << "Parsing failed\n";
+			std::cout << "*** Parsing failed ***\n";
 		}
     }
 
