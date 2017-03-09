c6a8ec418f63deb7e93593d97833baff91943251
Olaf <olaf@home.lan>
2017-03-09 20:29:44 +0100

    fixed for > int_max

--- playground/vhdl93/x3_integer.cpp
+++ playground/vhdl93/x3_integer.cpp
@@ -28,141 +28,117 @@
 #include <boost/spirit/home/x3.hpp>
 
 #include <iostream>
-#include <vector>
-#include <algorithm> // for copy
-#include <iterator>  // for ostream_iterator
-#include <sstream>
-
-#include <boost/type_index.hpp>
-
 
 
 namespace parser {
 
-  namespace x3 = boost::spirit::x3;
-  namespace iso8859_1 = boost::spirit::x3::iso8859_1;
+   namespace x3 = boost::spirit::x3;
 
-  using iso8859_1::char_;
+   using x3::char_;
 
-  typedef x3::rule<struct integer_class, uint32_t> integer_type;
+   typedef x3::rule<struct integer_class, uint32_t> integer_type;
 
-  integer_type const integer { "integer" };
+   integer_type const integer { "integer" };
 
-  auto print_type_of = [](auto &ctx) {
-     std::cout << boost::typeindex::type_id<decltype(ctx)>().pretty_name() << "\n";
-  };
+   constexpr uint32_t digit_treshold(auto i) {
+       return std::numeric_limits<decltype(i)>::max() % 10;
+   }
 
-  constexpr uint32_t digit_treshold(auto i) {
-      return std::numeric_limits<decltype(i)>::max() % 10;
-  };
-
-  auto const combine = [](auto &ctx) {
-      //typedef decltype(x3::_(ctx)) base_type;
-      typedef uint32_t base_type;
-      base_type result { 0 };
-      uint32_t iter_cnt { 0 };
-      for (auto&& ch : x3::_attr(ctx)) {
-          switch (ch) {
-              case '_': break;
-              default:
-                  if(iter_cnt > std::numeric_limits<base_type>::digits10) {
-                      x3::_pass(ctx) = false;
-                      continue;
-                  }
-                  if(iter_cnt++ < std::numeric_limits<base_type>::digits10) {
-                      result = result*10 + (ch - '0');
-                      //std::cout << "> c = " << iter_cnt << ", r = " << result << '\n';
-                  }
-                  else {
+   auto const combine = [](auto &ctx) {
+       //typedef decltype(x3::_attr(ctx)) base_type; // =>> templated?
+       typedef uint32_t base_type;
+       base_type result { 0 };
+       uint32_t iter_cnt { 0 };
+       for (auto&& ch : x3::_attr(ctx)) {
+           switch (ch) {
+               case '_': break;
+               default:
+                   if(iter_cnt > std::numeric_limits<base_type>::digits10) {
+                       x3::_pass(ctx) = false;
+                       continue;
+                   }
+                   if(iter_cnt++ <
+std::numeric_limits<base_type>::digits10) {
+                       result = result*10 + (ch - '0');
+                   }
+                   else {
 #if 0
-                      switch(ch - '0') {
-                      case 0: // [[fallthrough]];
-                      case 1: // [[fallthrough]];
-                      case 2: // [[fallthrough]];
-                      case 3: // [[fallthrough]];
-                      case 4: // [[fallthrough]];
-                      case 5:
-                          result = result*10 + (ch - '0');
-                          //std::cout << ". c = " << iter_cnt << ", r = " << result << '\n';
-                          break;
-                      default:
-                          x3::_pass(ctx) = false;
-                      }
+                       switch(ch - '0') {
+                       case 0: // [[fallthrough]]
+                       case 1: // [[fallthrough]]
+                       case 2: // [[fallthrough]]
+                       case 3: // [[fallthrough]]
+                       case 4: // [[fallthrough]]
+                       case 5:
+                           result = result*10 + (ch - '0');
+                           break;
+                       default:
+                           x3::_pass(ctx) = false;
+                       }
 #else
-                      if((ch - '0') > digit_treshold(result)) { // FixMe: > trshhld! erspart +1 in constexpr()
-                    	  x3::_pass(ctx) = false;
-                      }
-                      else {
-                    	  result = result*10 + (ch - '0');
-                      }
+                       if((unsigned)(ch - '0') > digit_treshold(result)) {
+                          x3::_pass(ctx) = false;
+                       }
+                       else {
+                          result = result*10 + (ch - '0');
+                       }
 #endif
-                  }
-          }
-      }
+                   }
+           }
+       }
 
-      x3::_val(ctx) = result;
-  };
+       x3::_val(ctx) = result;
+   };
 
-  // Parse '1_000' as 1000
-  auto const integer_def =
-    x3::lexeme [ +char_("0-9_") ] [combine]
-    ;
+   // Parse '1_000' as 1000
+   auto const integer_def =
+     //x3::lexeme [ char_("0-9") >> *(char_("0-9_")) ] [combine] /* _val(ctx) => uint */
+     x3::lexeme [ +char_("0-9_") ] [combine]
+     ;
 
-  BOOST_SPIRIT_DEFINE(integer)
+   BOOST_SPIRIT_DEFINE(integer)
 }
 
 
 int main()
 {
-  namespace x3 = boost::spirit::x3;
+   namespace x3 = boost::spirit::x3;
 
-  std::vector<std::string> const test_cases {
-      "0",
-      "1",
-      "01",
-      "1_000",
-      "42_666_4711",
-      "4_294_967_295", // uint32::max
-      "4_294_967_296",
-      "4_294_967_295_0",
-      "4_294_967_295_00",
-      };
+   std::vector<std::string> const test_cases {
+       "0",
+       "1",
+       "01",
+       "1_000",
+       "42_666_4711",
+       "4_294_967_295", // uint32::max
+       // below must fail to parse
+       "4_294_967_296",
+       "4_294_967_295_0",
+       "4_294_967_295_00",
+       "_42", "42_",
+       " 4 2 ", "4 _2"
+       };
 
-  typedef std::string::const_iterator iterator_type;
+   typedef std::string::const_iterator iterator_type;
 
-  for(auto str: test_cases) {
-    iterator_type iter = str.begin();
-    iterator_type const end = str.end();
+   for(auto str: test_cases) {
+     iterator_type iter = str.begin();
+     iterator_type const end = str.end();
 
-    auto& rule = parser::integer;
+     auto& rule = parser::integer;
 
-    std::cout << "parse `" << str << "´:\n";
+     std::cout << "parse `" << str << "Â´:\n";
 
-    uint32_t i;
+     uint32_t i;
 
-    bool r = x3::phrase_parse(iter, end, rule, x3::space, i);
+     bool r = x3::phrase_parse(iter, end, rule, x3::space, i);
 
-    if (r && iter == end) {
-      std::cout << "succeeded: '" << str << "' -> " << i << "\n";
-    } else {
-      std::cout << "failed\n";
-    }
-  }
+     if (r && iter == end) {
+       std::cout << "succeeded: \"" << str << "\" -> " << i << "\n";
+     } else {
+       std::cout << "failed\n";
+     }
+   }
 
-  return 0;
+   return 0;
 }
-
-
-/*
-boost::spirit::x3::context<boost::spirit::x3::attr_context_tag, std::vector<int, std::allocator<int> >, 
-boost::spirit::x3::context<boost::spirit::x3::where_context_tag, boost::iterator_range<__gnu_cxx::__normal_iterator<char const*, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > > >, 
-boost::spirit::x3::context<boost::spirit::x3::rule_val_context_tag, std::vector<int, std::allocator<int> >, 
-boost::spirit::x3::context<boost::spirit::x3::parse_pass_context_tag, bool, boost::spirit::x3::context<boost::spirit::x3::skipper_tag, boost::spirit::x3::char_class<boost::spirit::char_encoding::standard, boost::spirit::x3::space_tag> const, boost::spirit::x3::unused_type> > > > >
-*/
-
-/* type_of()
-boost::spirit::x3::context<boost::spirit::x3::attr_context_tag, int, 
-boost::spirit::x3::context<boost::spirit::x3::where_context_tag, boost::iterator_range<__gnu_cxx::__normal_iterator<char const*, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > > >, 
-boost::spirit::x3::context<boost::spirit::x3::rule_val_context_tag, std::vector<int, std::allocator<int> >, 
-boost::spirit::x3::context<boost::spirit::x3::parse_pass_context_tag, bool, boost::spirit::x3::context<boost::spirit::x3::skipper_tag, boost::spirit::x3::char_class<boost::spirit::char_encoding::standard, boost::spirit::x3::space_tag> const, boost::spirit::x3::unused_type> > > > >
-*/
