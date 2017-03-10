24d2e5b654ce2e55d2de3f1a5fa53330fe49b90b
Olaf <olaf@home.lan>
2017-03-10 15:53:38 +0100

    working version

--- playground/vhdl93/x3_integer.cpp
+++ playground/vhdl93/x3_integer.cpp
@@ -12,6 +12,21 @@
 
 #include <iostream>
 
+#include <boost/type_index.hpp>
+
+#include <boost/fusion/algorithm/iteration/for_each.hpp>
+#include <boost/fusion/include/for_each.hpp>
+#include <boost/fusion/container/vector/convert.hpp>
+#include <boost/fusion/include/as_vector.hpp>
+
+#include <boost/integer.hpp>
+
+/* -------------------------------------------------------------------------*/
+auto print_type_of = [](auto &ctx) {
+   std::cout << boost::typeindex::type_id<decltype(ctx)>().pretty_name() << "\n";
+};
+/* -------------------------------------------------------------------------*/
+
 
 namespace parser {
 
@@ -23,25 +38,27 @@ namespace parser {
 
    integer_type const integer { "integer" };
 
+   //  warning: ISO C++ forbids use of 'auto' in parameter declaration [-Wpedantic]
    constexpr uint32_t digit_treshold(auto i) {
        return std::numeric_limits<decltype(i)>::max() % 10;
    }
 
    auto const combine = [](auto &ctx) {
-       //typedef decltype(x3::_attr(ctx)) base_type; // =>> templated?
-       typedef uint32_t base_type;
-       base_type result { 0 };
+       //typedef decltype(x3::_attr(ctx)) value_type; // =>> templated?
+       typedef uint32_t value_type;
+       typedef boost::uint_t<sizeof(value_type)*8+1>::fast acc_type;
+       acc_type result { 0 };
        uint32_t iter_cnt { 0 };
        for (auto&& ch : x3::_attr(ctx)) {
            switch (ch) {
                case '_': break;
                default:
-                   if(iter_cnt > std::numeric_limits<base_type>::digits10) {
+                   if(iter_cnt > std::numeric_limits<value_type>::digits10) {
                        x3::_pass(ctx) = false;
                        continue;
                    }
                    if(iter_cnt++ <
-std::numeric_limits<base_type>::digits10) {
+std::numeric_limits<value_type>::digits10) {
                        result = result*10 + (ch - '0');
                    }
                    else {
@@ -82,6 +99,51 @@ std::numeric_limits<base_type>::digits10) {
    BOOST_SPIRIT_DEFINE(integer)
 }
 
+namespace parser2 {
+
+   namespace x3 = boost::spirit::x3;
+
+   using x3::char_;
+
+   auto const combine_to_uint = [](auto &ctx) {
+
+       namespace fu = boost::fusion;
+       
+       // Attr type is fusion::deque<char, std::string<char,...>>
+       //std::cout << "Val: " << boost::typeindex::type_id<decltype(x3::_val(ctx))>().pretty_name() << "\n";
+       //std::cout << "Attr: " << boost::typeindex::type_id<decltype(x3::_attr(ctx))>().pretty_name() << "\n";
+
+       typedef typename std::decay<decltype(x3::_val(ctx))>::type value_type;
+       typedef typename boost::uint_t<sizeof(value_type)*8+1>::fast acc_type;
+       
+       static_assert(sizeof(value_type) < sizeof(acc_type), "Accumulator to small");
+       
+       auto as_uint = [](auto ch) { return static_cast<uint>(ch - '0'); };
+       
+       acc_type acc { as_uint(fu::at_c<0>(x3::_attr(ctx))) };
+       
+        for (auto&& ch : fu::at_c<1>(x3::_attr(ctx))) { 
+            switch (ch) {
+                case '_': break;
+                default:  acc = acc*10 + as_uint(ch);
+            }
+            if(acc > std::numeric_limits<value_type>::max()) {
+                x3::_pass(ctx) = false;
+            }
+        }
+        
+        x3::_val(ctx) = static_cast<value_type>(acc);
+   };
+
+   typedef x3::rule<struct _, uint32_t> integer_type;
+   integer_type const integer {};
+   // Parse '1_000' as 1000
+   auto const integer_def = x3::lexeme [
+       (char_("0-9") >> *char_("0-9_")) 
+    ] [combine_to_uint] ;
+
+   BOOST_SPIRIT_DEFINE(integer)
+}
 
 int main()
 {
@@ -98,8 +160,8 @@ int main()
        "4_294_967_296",
        "4_294_967_295_0",
        "4_294_967_295_00",
-       "_42", "42_",
-       " 4 2 ", "4 _2"
+       "_42", 
+	   "42_", // shouldn't pass !
        };
 
    typedef std::string::const_iterator iterator_type;
@@ -108,9 +170,9 @@ int main()
      iterator_type iter = str.begin();
      iterator_type const end = str.end();
 
-     auto& rule = parser::integer;
+     auto& rule = parser2::integer;
 
-     std::cout << "parse `" << str << "Â´:\n";
+     std::cout << "parse `" << str << "´: ";
 
      uint32_t i;
 
@@ -119,7 +181,7 @@ int main()
      if (r && iter == end) {
        std::cout << "succeeded: \"" << str << "\" -> " << i << "\n";
      } else {
-       std::cout << "failed\n";
+       std::cout << "*** failed ***\n";
      }
    }
 
