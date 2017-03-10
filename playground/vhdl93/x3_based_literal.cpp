85272142d80d0053c536579f38dbe7b1e846ec64
Olaf <olaf@home.lan>
2017-03-10 21:47:31 +0100

    initial version of based_literal

--- playground/vhdl93/x3_based_literal.cpp
+++ playground/vhdl93/x3_based_literal.cpp
@@ -1,9 +1,10 @@
-#include <iosfwd>
+#include <iostream>
 //#define BOOST_SPIRIT_X3_DEBUG
 
 #include <boost/spirit/home/x3.hpp>
-
-#include <iostream>
+#include <boost/optional/optional_io.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/fusion/include/io.hpp>
 
 #include <boost/type_index.hpp>
 
@@ -12,16 +13,23 @@
 
 #include <boost/integer.hpp>
 
-/* -------------------------------------------------------------------------*/
-auto print_type_of = [](auto &ctx) {
-   std::cout << boost::typeindex::type_id<decltype(ctx)>().pretty_name() << "\n";
-};
-/* -------------------------------------------------------------------------*/
+#include <algorithm>
+#include <iterator>
 
 
-namespace parser2 {
+namespace ast {
+
+    struct based_integer {
+        char    extended_digit;
+    };
+
+    using boost::fusion::operator<<;
+}
+
+namespace parser {
 
    namespace x3 = boost::spirit::x3;
+   namespace fu = boost::fusion;
 
    using x3::char_;
 
@@ -29,55 +37,155 @@ namespace parser2 {
 
        namespace fu = boost::fusion;
        
-       // Attr type is fusion::deque<char, std::string<char,...>>
-       std::cout << "Val: " << boost::typeindex::type_id<decltype(x3::_val(ctx))>().pretty_name() << "\n";
+       /*
+        * Val: std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >
+        * Attr: boost::fusion::deque<char, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > >
+        */
+       std::cout << "\nVal: " << boost::typeindex::type_id<decltype(x3::_val(ctx))>().pretty_name() << "\n";
        std::cout << "Attr: " << boost::typeindex::type_id<decltype(x3::_attr(ctx))>().pretty_name() << "\n";
 
-        
+       auto const inner_attr = [](auto& i) {
+           std::cout << "InnerAttr: " << boost::typeindex::type_id<i>().pretty_name() << "\n";
+       };
+
+       //fu::for_each(x3::_attr(ctx), inner_attr);
         //x3::_val(ctx) = static_cast<value_type>(acc);
    };
 
-   typedef x3::rule<struct _, x3::unused_type> based_literal_type;
-   based_literal_type const based_literal {};
-    // based_literal ::=
-	// base # based_literal [ . based_literal ] # [ exponent ]
+   auto const integer = x3::rule<struct integer_class, uint> { "integer" } =
+           x3::uint_
+           ;
 
-   auto const based_literal_def = x3::lexeme [
-       //(+x3::uint_ > '#'  > +x3::uint_ ) >> -( '.' > +x3::uint_) > '#' >> -x3::uint_
-       +x3::uint_ >> '#' >> +x3::uint_  >>  '.'// >> +x3::uint_ //>> '#' >> -x3::uint_
-    ] [show_me] ;
-;
-   BOOST_SPIRIT_DEFINE(based_literal)
+   auto const base = x3::rule<struct base_class, uint> { "base" } =
+           integer
+           ;
+
+   auto const extended_digit = x3::rule<struct extended_digit_class, char> { "extended_digit" } =
+           char_("0-9") | char_("A-Fa-f")
+           ;
+
+   auto const underline = x3::rule<struct underline_class> { "underline" } = char_('_');
+   auto const based_integer = x3::rule<struct based_integer_class, std::string> { "based_integer" } =
+           x3::lexeme[
+                  extended_digit >> *( -x3::lit('_') >> extended_digit )
+           ] //[ show_me]
+           ;
+
+   auto signum_pos = [](auto& ctx) { x3::_val(ctx) = false; };
+   auto signum_neg = [](auto& ctx) { x3::_val(ctx) = true;  };
+   auto const signum = x3::rule<struct signum_class, bool> { "signum" } =
+             x3::lit('+')[ signum_pos ]
+           | x3::lit('-')[ signum_neg ]
+           | x3::eps[ signum_pos]
+           ;
+
+   auto combine_exp = [](auto& ctx) {
+       x3::_val(ctx) = fu::at_c<0>(x3::_attr(ctx)) ? -fu::at_c<1>(x3::_attr(ctx)) : fu::at_c<1>(x3::_attr(ctx));
+   };
+   auto const exponent = x3::rule<struct exponent_class, int32_t> { "exponent" } =
+           x3::lexeme [
+                   (x3::lit("E") | "e") >> signum >> integer
+           ] [combine_exp]
+           ;
+
+   auto const based_literal = x3::rule<struct _, std::string> {} =
+           x3::lexeme [
+
+                  base >> '#' >> based_integer /* .... */ >> '#' >> exponent
+           ]// [show_me]
+           ;
 }
+
 
 int main()
 {
    namespace x3 = boost::spirit::x3;
 
-   std::vector<std::string> const test_cases {
-       "2#1111_1111#",
-       "16#FF#",
-       "016#0FF#",
-       };
-
    typedef std::string::const_iterator iterator_type;
 
-   for(auto str: test_cases) {
-     iterator_type iter = str.begin();
-     iterator_type const end = str.end();
 
-     auto& rule = parser2::based_literal;
+   {
+       std::vector<std::string> const test_cases {
+           "E3",
+           "e42",
+           "E2147483647",  // INT_MAX
+           "E+2147483647", // INT_MAX
+           "E-2147483648", // INT_MIN
+           "E-0",
+           };
 
-    // std::cout << "parse `" << str << "´: ";
+       for(auto str: test_cases) {
+         iterator_type iter = str.begin();
+         iterator_type const end = str.end();
 
-     bool r = x3::phrase_parse(iter, end, rule, x3::space);
+         auto& rule = parser::exponent;
+         int32_t attr;
 
-     if (r && iter == end) {
-       std::cout << "succeeded: \"" << str << "\n";
-     } else {
-       //::cout << "*** failed ***\n";
-     }
+         std::cout << "parse `" << str << ": ";
+
+         bool r = x3::phrase_parse(iter, end, rule, x3::space, attr);
+
+         if (r && iter == end) {
+           std::cout << "succeeded: " << attr;
+           std::cout << '\n';
+         } else {
+           std::cout << "*** failed ***\n";
+         }
+       }
+   }
+   {
+       std::vector<std::string> const test_cases {
+           " 1111_1111 ",
+           " AA_FF_FF_EE"
+           };
+
+       for(auto str: test_cases) {
+         iterator_type iter = str.begin();
+         iterator_type const end = str.end();
+
+         auto& rule = parser::based_integer;
+         std::string attr;
+
+         std::cout << "parse `" << str << ": ";
+
+         bool r = x3::phrase_parse(iter, end, rule, x3::space, attr);
+
+         if (r && iter == end) {
+           std::cout << "succeeded:\n";
+           std::copy(attr.begin(), attr.end(), std::ostream_iterator<boost::optional<char>>(std::cout, ", "));
+           std::cout << '\n';
+         } else {
+           std::cout << "*** failed ***\n";
+         }
+       }
+   }
+   {
+       std::vector<std::string> const test_cases {
+           " 4711#1111_1111#E65 ",
+           " 42#AA_FF_FF_EE#e42"
+           };
+
+       for(auto str: test_cases) {
+         iterator_type iter = str.begin();
+         iterator_type const end = str.end();
+
+         auto& rule = parser::based_literal;
+         std::string attr;
+
+         std::cout << "parse `" << str << ": ";
+
+         bool r = x3::phrase_parse(iter, end, rule, x3::space, attr);
+
+         if (r && iter == end) {
+           std::cout << "succeeded:\n";
+           std::copy(attr.begin(), attr.end(), std::ostream_iterator<boost::optional<char>>(std::cout, ", "));
+           std::cout << '\n';
+         } else {
+           std::cout << "*** failed ***\n";
+         }
+       }
    }
 
    return 0;
 }
+
