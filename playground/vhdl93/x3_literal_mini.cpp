6f5850666fd05dbef79c008e4a4bd723a90e971d
Olaf <olaf@home.lan>
2017-03-11 14:03:38 +0100

    even more minimaized

--- playground/vhdl93/x3_literal_mini.cpp
+++ playground/vhdl93/x3_literal_mini.cpp
@@ -29,37 +29,25 @@ BOOST_FUSION_ADAPT_STRUCT(
 namespace parser {
 
    namespace x3 = boost::spirit::x3;
-   namespace fu = boost::fusion;
 
    using x3::char_;
    using x3::lit;
    using x3::int_;
 
-   auto const part = x3::rule<struct based_integer_class, std::string> { "part" } =
+   auto const part = x3::rule<struct _, std::string> {} =
            x3::lexeme[ +char_("0-9") ]
            ;
 
-   auto signum_pos = [](auto& ctx) { x3::_val(ctx) = false; };
-   auto signum_neg = [](auto& ctx) { x3::_val(ctx) = true;  };
-   auto const signum = x3::rule<struct signum_class, bool> { "signum" } =
-             lit('+')[ signum_pos ]
-           | lit('-')[ signum_neg ]
-           | x3::eps[ signum_pos]
-           ;
-
-   auto combine_exp = [](auto& ctx) {
-       x3::_val(ctx) = fu::at_c<0>(x3::_attr(ctx)) ? -fu::at_c<1>(x3::_attr(ctx)) : fu::at_c<1>(x3::_attr(ctx));
-   };
-   auto const exponent = x3::rule<struct exponent_class, int32_t> { "exponent" } =
+   auto const exponent = x3::rule<struct _, int32_t> {} =
            (x3::lexeme [
-                      (lit("E") | "e") >> signum >> int_
-           ][combine_exp]
+                      (lit("E") | "e") >> int_
+           ]
            | x3::attr(1)) // doesn't work???
            ;
 
-   auto const literal = x3::rule<struct _, std::string> { "literal" } =
+   auto const literal = x3::rule<struct _, std::string> {} =
            x3::lexeme [
-                  // int             string   optional<string>        int
+                  // int         string   optional<string>    int
                   int_ >> '#' >> part >> -('.' >> part) >> '#' >> exponent
            ]
            ;
@@ -78,13 +66,14 @@ int main()
      auto iter = str.begin(), end = str.end();
 
      ast::literal attr;
-     bool r = x3::phrase_parse(iter, end, parser::literal, x3::space /*, attr */);
+     bool r = x3::phrase_parse(iter, end, parser::literal, x3::space, attr);
 
      if (r && iter == end) {
-       std::cout << "succeeded:\n";
+       std::cout << "succeeded: " << attr << "\n";
      } else {
        std::cout << "*** failed ***\n";
      }
+   }
 
    return 0;
 }
