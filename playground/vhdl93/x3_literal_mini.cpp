5e567d92dbe18a8b6ee7a0ff2f7ed9470e36cd7d
Olaf <olaf@home.lan>
2017-03-11 13:51:21 +0100

    even more minimal

--- playground/vhdl93/x3_literal_mini.cpp
+++ playground/vhdl93/x3_literal_mini.cpp
@@ -6,7 +6,6 @@
 #include <boost/fusion/include/adapt_struct.hpp>
 #include <boost/fusion/include/io.hpp>
 
-
 namespace ast {
 
     struct literal {
@@ -33,6 +32,8 @@ namespace parser {
    namespace fu = boost::fusion;
 
    using x3::char_;
+   using x3::lit;
+   using x3::int_;
 
    auto const part = x3::rule<struct based_integer_class, std::string> { "part" } =
            x3::lexeme[ +char_("0-9") ]
@@ -41,8 +42,8 @@ namespace parser {
    auto signum_pos = [](auto& ctx) { x3::_val(ctx) = false; };
    auto signum_neg = [](auto& ctx) { x3::_val(ctx) = true;  };
    auto const signum = x3::rule<struct signum_class, bool> { "signum" } =
-             x3::lit('+')[ signum_pos ]
-           | x3::lit('-')[ signum_neg ]
+             lit('+')[ signum_pos ]
+           | lit('-')[ signum_neg ]
            | x3::eps[ signum_pos]
            ;
 
@@ -51,7 +52,7 @@ namespace parser {
    };
    auto const exponent = x3::rule<struct exponent_class, int32_t> { "exponent" } =
            (x3::lexeme [
-                      (x3::lit("E") | "e") >> signum >> x3::int_
+                      (lit("E") | "e") >> signum >> int_
            ][combine_exp]
            | x3::attr(1)) // doesn't work???
            ;
@@ -59,7 +60,7 @@ namespace parser {
    auto const literal = x3::rule<struct _, std::string> { "literal" } =
            x3::lexeme [
                   // int             string   optional<string>        int
-                  x3::int_ >> '#' >> part >> -('.' >> part) >> '#' >> exponent
+                  int_ >> '#' >> part >> -('.' >> part) >> '#' >> exponent
            ]
            ;
 }
@@ -69,33 +70,21 @@ int main()
 {
    namespace x3 = boost::spirit::x3;
 
-   typedef std::string::const_iterator iterator_type;
+   for(std::string const str: {
+           "4711#11111111.32#e42",
+           "4711#11111111.32#E-65",
+           "4711#11112222#", // why is attr<3> = 0, expected = 1 ???
+   }) {
+     auto iter = str.begin(), end = str.end();
 
-   std::vector<std::string> const test_cases {
-       "4711#11111111.32#e42",
-       "4711#11111111.32#E-65",
-       "4711#11112222#", // why is attr<3> = 0, expected = 1 ???
-       };
-
-   for(auto str: test_cases) {
-     iterator_type iter = str.begin();
-     iterator_type const end = str.end();
-
-     auto& rule = parser::literal;
      ast::literal attr;
-
-     std::cout << "parse `" << str << ": ";
-
-     bool r = x3::phrase_parse(iter, end, rule, x3::space /*, attr */);
+     bool r = x3::phrase_parse(iter, end, parser::literal, x3::space /*, attr */);
 
      if (r && iter == end) {
        std::cout << "succeeded:\n";
-
-       std::cout << '\n';
      } else {
        std::cout << "*** failed ***\n";
      }
-   }
 
    return 0;
 }
