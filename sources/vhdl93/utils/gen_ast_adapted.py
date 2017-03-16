8a687dd0f1b18547ca13e7cf1d1be7456fae0df9
Olaf <olaf@home.lan>
2017-03-16 16:57:28 +0100

    code review - factoring out common functions for re-use

--- sources/vhdl93/utils/gen_ast_adapted.py
+++ sources/vhdl93/utils/gen_ast_adapted.py
@@ -7,10 +7,13 @@ from pygccxml import parser
 import os
 import sys
 
+from collections import OrderedDict
+
 
 class AstFusionAdapter:
     global_namespace = None
     ns_ast = None
+    node_list = []
     width = 40
     indent = "    "
     
@@ -20,12 +23,13 @@ class AstFusionAdapter:
             xml_generator_path=generator_path,
             xml_generator=generator_name,
             cflags='-std=c++11')
-        header_list = [self.include_prefix() + hpp for hpp in self.find_ast_hpp()]
+        header_list = [self.include_prefix() + hpp for hpp in self.find_hpp()]
         decls = parser.parse(header_list, xml_generator_config)
         self.global_namespace = declarations.get_global_namespace(decls)
         self.ns_ast = self.global_namespace.namespace("ast")
+        self.update_db()
         
-    def find_ast_hpp(self):
+    def find_hpp(self):
         ast_include_path = self.include_prefix()
         file_ext = ['hpp']
         header_list = [hxx for hxx in os.listdir(ast_include_path)
@@ -43,24 +47,39 @@ class AstFusionAdapter:
         prefix = selfy + "/../include/eda/vhdl93" + '/ast' + '/'
         return prefix
         
-    def fusion_adapt(self, cls):
-        alist = []
-        alist.append(str(cls.decl_string).lstrip('::') + ',')
+    def nod_signature(self, cls):
+        '''
+        Extract the name of the ast node self and member variables. The order of
+        structure members matters here for BOOST_FUSION_ADAPT_STRUCT macro!
+        '''
+        member_dict = OrderedDict()
         members = [m for m in cls.get_members(access='public') if m.is_artificial == False]
         for member in members:
             if declarations.is_enum(member):
                 continue
-            alist.append("({0:<{1}}, {2:>{3}})".format(
-                member.decl_type, self.width, member.name, self.width/2))
-        text = "\n".join(self.indent + line for line in alist)
-        return "BOOST_FUSION_ADAPT_STRUCT(\n{0}\n)\n".format(text)
-
-    
-    def inspect(self):
-        text = ""
+            else:
+                member_dict.update({ str(member.decl_type) : member.name })
+        return (cls.decl_string, member_dict)
+        
+    def update_db(self):
         cls_list = [cls for cls in self.ns_ast.classes() if cls.class_type == 'struct']
         for cls in cls_list:
-            text += self.fusion_adapt(cls) + '\n'
+            self.node_list.append(self.nod_signature(cls))
+            
+    def fusion_adapt(self, node):
+        node_name = node[0]
+        attr_dict = node[1]
+        alist = [node_name.lstrip('::') + ',']
+        for attr in attr_dict.items():
+            alist.append("({0:<{1}}, {2:>{3}})".format(
+                attr[0], self.width, attr[1], self.width/2))
+        text = "\n".join(self.indent + line for line in alist)
+        return "BOOST_FUSION_ADAPT_STRUCT(\n{0}\n)\n".format(text)   
+            
+    def fusionize(self):
+        text = ""
+        for node in self.node_list:
+            text += self.fusion_adapt(node) + '\n'
         return text
     
     def print_all(self):
@@ -69,7 +88,8 @@ class AstFusionAdapter:
     
 if __name__ == "__main__":
     adapter = AstFusionAdapter()
-    print(adapter.inspect())
+    print(adapter.fusionize())
+    
     
     #adapter.print_all()
 
