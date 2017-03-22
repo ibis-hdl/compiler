6b6b65d7060c7037f2817c7559d1d340712dfee4
Olaf <olaf@home.lan>
2017-03-22 16:09:55 +0100

    cmake and bash cosmetic

--- eclipse-cmake.sh
+++ eclipse-cmake.sh
@@ -1,8 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-SRC_DIR=../IBIS_SOURCE/
+# http://mywiki.wooledge.org/BashFAQ/028
+SCRIPT_PATH="${BASH_SOURCE%/*}"
+cd "${SCRIPT_PATH}" || exit
 
-cmake -G"Eclipse CDT4 - Unix Makefiles" \
+# this script is in the same directory
+EDA_SRC_DIR=${SCRIPT_PATH}
+
+
+cmake -G "Eclipse CDT4 - Unix Makefiles" \
         -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE \
         -DCMAKE_ECLIPSE_VERSION=4.6 \
         -DCMAKE_ECLIPSE_MAKE_ARGUMENTS=-j4 \
@@ -10,7 +16,10 @@ cmake -G"Eclipse CDT4 - Unix Makefiles" \
         -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
         -DCMAKE_BUILD_TYPE=Debug \
         -DCMAKE_CXX_COMPILER=g++ \
-        ${SRC_DIR}
-
+        -DCMAKE_INSTALL_PREFIX=stage
+        -DBUILD_SHARED_LIBS=OFF
+        ${EDA_SRC_DIR}
+        
+#cmake --build .
 make -j4
 
