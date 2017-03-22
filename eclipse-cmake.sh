afbf8774e48065cd88d17672e1a28c433eaeeade
Olaf <olaf@home.lan>
2017-03-22 16:15:37 +0100

    fix line break

--- eclipse-cmake.sh
+++ eclipse-cmake.sh
@@ -7,7 +7,6 @@ cd "${SCRIPT_PATH}" || exit
 # this script is in the same directory
 EDA_SRC_DIR=${SCRIPT_PATH}
 
-
 cmake -G "Eclipse CDT4 - Unix Makefiles" \
         -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE \
         -DCMAKE_ECLIPSE_VERSION=4.6 \
@@ -16,8 +15,8 @@ cmake -G "Eclipse CDT4 - Unix Makefiles" \
         -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
         -DCMAKE_BUILD_TYPE=Debug \
         -DCMAKE_CXX_COMPILER=g++ \
-        -DCMAKE_INSTALL_PREFIX=stage
-        -DBUILD_SHARED_LIBS=OFF
+        -DCMAKE_INSTALL_PREFIX=stage \
+        -DBUILD_SHARED_LIBS:BOOL=OFF \
         ${EDA_SRC_DIR}
         
 #cmake --build .
