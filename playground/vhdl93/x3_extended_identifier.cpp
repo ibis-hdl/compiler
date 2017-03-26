4de8ca7e511e055b414702f380e4a2c4afb6a871
Olaf <olaf@home.lan>
2017-03-26 07:50:18 +0200

    not fixed yet, working on

--- playground/vhdl93/x3_extended_identifier.cpp
+++ playground/vhdl93/x3_extended_identifier.cpp
@@ -25,13 +25,17 @@ namespace parser {
 		char_("A-Za-z") >> *( -lit("_") >> +char_("A-Za-z0-9") )
 		;
 
+   auto const extended_identifier_str = x3::rule<struct _, std::string> { "extended_identifier (inner)" } =
+        +( char_ - char_('\\') )
+        ;
+
    auto const extended_identifier = x3::rule<struct _, std::string> { "extended_identifier" } =
 		lexeme [
-			   '\\'
-			>> ( ( char_ >> *( char_ - char_('\\') ) )
-			   | ( char_('\\') >> char_('\\'))
+			   char_('\\')
+			>> ( extended_identifier_str
+			   | extended_identifier_str % ( char_('\\') >> char_('\\') )
 			   )
-			>> '\\'
+			>> char_('\\')
 		]
 		;
 
@@ -40,28 +44,23 @@ namespace parser {
    		;
 }
 
+
 int main()
 {
    namespace x3 = boost::spirit::x3;
 
-   typedef std::string::const_iterator iterator_type;
+   for(std::string const str: {
+	   "\\BoBus\\",			// expected '\Bus\'
+	   "\\Foo\\\\oh\\", 	// expected '\Foo\\oh\'
+    })
+    {
+	 auto iter = str.begin(), end = str.end();
 
-   std::vector<std::string> const test_cases {
-	   "\\Bus\\",
-	   "\\Foo\\\\oh\\",
-	   //"VHDL"
-	   };
-
-   for(auto str: test_cases) {
-	 iterator_type iter = str.begin();
-	 iterator_type const end = str.end();
-
-	 auto& rule = parser::extended_identifier;
 	 std::string attr;
 
 	 std::cout << "parse `" << str << "`:\n";
 
-	 bool r = x3::phrase_parse(iter, end, rule, x3::space, attr);
+	 bool r = x3::phrase_parse(iter, end, parser::identifier, x3::space, attr);
 
 	 if (r && iter == end) {
 	   std::cout << "succeeded: \"" << attr << "\"\n";
@@ -73,6 +72,4 @@ int main()
 
    return 0;
 }
-
-
 
