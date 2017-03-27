d2a38137d45b5004d67b9dba960a3586f790df2c
Olaf <olaf@home.lan>
2017-03-27 19:42:31 +0200

    added test mock for argc/argv

--- sources/vhdl93/test/test_vhdl93.cpp
+++ sources/vhdl93/test/test_vhdl93.cpp
@@ -9,3 +9,10 @@
 #define BOOST_TEST_MODULE "VHDL93 Parser Test Suite"
 #include <boost/test/included/unit_test.hpp>
 
+#include "app_mock.hpp"
+
+app_mock::app_mock()
+: argc(boost::unit_test::framework::master_test_suite().argc)
+, argv(boost::unit_test::framework::master_test_suite().argv)
+{ }
+
