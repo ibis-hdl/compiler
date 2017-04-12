4f3b93eb0730f53b5e468f0878bce1356e09d992
Olaf <olaf@home.lan>
2017-04-12 20:31:45 +0200

    fix for attribute instance
    - not a class member any more, created each time new on stack

--- sources/vhdl93/test/testing_parser.hpp
+++ sources/vhdl93/test/testing_parser.hpp
@@ -25,72 +25,63 @@ namespace parser = eda::vhdl93::parser;
 namespace ast    = eda::vhdl93::ast;
 
 
-template <typename AttrType = x3::unused_type, typename SkipperType = parser::skipper_type>
+template <
+    typename AttrType = x3::unused_type,
+    typename SkipperType = parser::skipper_type
+>
 struct testing_parser
 {
-	typedef AttrType					attribute_type;
-	typedef SkipperType					skipper_type;
-	attribute_type						attr;
-	skipper_type						skipper;
+    typedef AttrType                    attribute_type;
+    typedef SkipperType                 skipper_type;
+
+    skipper_type                        skipper;
 
 
-	template <typename ParserType>
-	std::tuple<bool, std::string>
-	operator()(
-	    std::string const &input,
-	    ParserType const &parser_rule,
-	    bool full_match = true
-	    )
-	{
-	    btt::output_test_stream output;
+    template <typename ParserType>
+    std::tuple<bool, std::string>
+    operator()(
+        std::string const &input,
+        ParserType const &parser_rule,
+        bool full_match = true
+        )
+    {
+        attribute_type  attr;
+        btt::output_test_stream output;
 
-	    parser::iterator_type iter = input.begin();
-	    parser::iterator_type const end = input.end();
+        parser::iterator_type iter = input.begin();
+        parser::iterator_type const end = input.end();
 
-	    parser::error_handler_type error_handler(iter, end, output);
+        parser::error_handler_type error_handler(iter, end, output);
 
-	    auto const parser =
-	        x3::with<x3::error_handler_tag>(std::ref(error_handler))
-	        [
-	              parser_rule
-	        ];
+        auto const parser =
+            x3::with<x3::error_handler_tag>(std::ref(error_handler))
+            [
+                  parser_rule
+            ];
 
-	    bool success =
-	      x3::phrase_parse(iter, end, parser, skipper, attr);
+        bool success =
+          x3::phrase_parse(iter, end, parser, skipper, attr);
 
-	    if (success) {
-	        if (iter != end) {
-	            error_handler(iter, "Error! Expecting end of input here: ");
-	        }
-	        else {
-	            ast::printer print(output);
-	            print.verbose_symbol = true;
-	            print.verbose_variant = true;
-	            print(attr);
-	        }
-	    }
+        if (success) {
+            if (iter != end) {
+                error_handler(iter, "Error! Expecting end of input here: ");
+            }
+            else {
+                ast::printer print(output);
+                print.verbose_symbol = true;
+                print.verbose_variant = true;
+                print(attr);
+            }
+        }
 
-	    // FixMe: C++17 Structured Bindings
-	    return std::make_tuple(
-	        success && (!full_match || (iter == end)),
-	        output.str()
-	    );
-	}
+        // FixMe: C++17 Structured Bindings
+        return std::make_tuple(
+            success && (!full_match || (iter == end)),
+            output.str()
+        );
+    }
 };
 
-
-
-
-//    class tester
-//    {
-//        fs::path prefix {
-//        fs::path("/home/olaf/work/CXX/IBIS_SOURCE/sources/vhdl93/test/literal")
-//        //fs::path(app_mock().argv[0]).filename()
-//    }; // FixMe: extra cmd args
-//
-//    unsigned num_files_tested { 0 };
-//
-//};
 
 } // namespace x3_test
 
