5a6041645bf7976f59127713fb97f055d7baabd6
Olaf <olaf@home.lan>
2017-04-12 19:37:31 +0200

    common_identifier refactored

--- sources/vhdl93/include/eda/vhdl93/parser/grammar_def.hpp
+++ sources/vhdl93/include/eda/vhdl93/parser/grammar_def.hpp
@@ -1070,29 +1070,42 @@ auto as_rule = [](auto p) { return x3::rule<struct _, T>{ "as" } = x3::as_parser
  *
  */
 
-// string_literal ::=                                                   [§ 13.6]
-// " { graphic_character } "
+
 namespace detail {
 
-auto const common_string_literal = x3::rule<struct _, std::string> { "string_literal" } =
-    lexeme[
-        (      '"'
-            >> *( ( graphic_character - '"'  )
-                | ( char_('"') >> char_('"') )
-                )
-            >> '"'
-        )
-        |
-        (      '%' // LRM [§ 13.10]
-            >> *( ( graphic_character - '%'  )
-                | ( char_('%') >> char_('%') )
-                )
-            >> '%'
-        )
-    ]
-    ;
+    /* The identifier is treated as string. The basic_identifier and
+     * extended_identifier can be distinguished simply by test on leading (and
+     * trailing) '/'. To enforce use of a single string member on AST node resp.
+     * identifier the rule is splitted, otherwise ast::identifier has to handle
+     * the variant type. Generalizing these identifiers simplifies (hopefully)
+     * the type/identifier handling (without applying the boost visitor). */
+    // identifier ::=                                                   [§ 13.3]
+    // basic_identifier | extended_identifier
+    auto const common_identifier = x3::rule<struct _, std::string> { "identifier" } =
+          basic_identifier
+        | extended_identifier
+        ;
 
 
+    // string_literal ::=                                               [§ 13.6]
+    // " { graphic_character } "
+    auto const common_string_literal = x3::rule<struct _, std::string> { "string_literal" } =
+        lexeme[
+            (      '"'
+                >> *( ( graphic_character - '"'  )
+                    | ( char_('"') >> char_('"') )
+                    )
+                >> '"'
+            )
+            |
+            (      '%' // LRM [§ 13.10]
+                >> *( ( graphic_character - '%'  )
+                    | ( char_('%') >> char_('%') )
+                    )
+                >> '%'
+            )
+        ]
+        ;
 }
 
 
@@ -2462,22 +2475,8 @@ auto const guarded_signal_specification_def =
 
 // identifier ::=                                                       [§ 13.3]
 // basic_identifier | extended_identifier
-namespace detail {
-
-    /* The identifier is treated as string. The basic_identifier and
-     * extended_identifier can be distinguished simply by test on leading (and
-     * trailing) '/'. To enforce use of a single string member on AST node resp.
-     * identifier the rule is splitted, otherwise ast::identifier has to handle
-     * the variant type. Generalizing these identifiers simplifies (hopefully)
-     * the type/identifier handling (without applying the boost visitor). */
-    auto const identifier_str = x3::rule<struct _, std::string> { "identifier" } =
-          basic_identifier
-        | extended_identifier
-        ;
-}
-
 auto const identifier_def =
-    detail::identifier_str
+    detail::common_identifier
     ;
 
 
@@ -3386,7 +3385,7 @@ auto const simple_expression_def =
 // simple_name ::=                                                       [§ 6.2]
 // identifier
 auto const simple_name_def =
-    detail::identifier_str
+    detail::common_identifier
     ;
 
 
@@ -3592,9 +3591,9 @@ auto const type_definition_def =
 //     | subtype_name
 /* Note, there is no way to distinguish between type_name and subtype_name at
  * parser level. It's used as simple string parser for symantic sugar at parser
- * level*/
+ * level exposing a std::string attribute. */
 auto const type_mark_def =
-    detail::identifier_str
+    detail::common_identifier
     ;
 
 
