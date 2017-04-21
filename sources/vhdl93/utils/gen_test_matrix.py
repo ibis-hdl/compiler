e00cd656d486ba04666c455140242a57660ff429
Olaf <olaf@home.lan>
2017-04-21 16:09:56 +0200

    using prettyprint for tables, working so far

--- sources/vhdl93/utils/gen_test_matrix.py
+++ sources/vhdl93/utils/gen_test_matrix.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import os
-from collections import namedtuple
+from prettytable import PrettyTable
+import prettytable
+
 
 class RstMaker:
     '''A simple StructuredText generator'''
@@ -12,82 +14,31 @@ class RstMaker:
     }
 
     def title_header(self, title, depth = 1):
-        adornment = ''
-        for n in title:
-            adornment += self.adornment.get(depth, '')
-        text='''\n{0}\n{1}\n{0}\n'''.format(adornment, title)
+        adornment = self.adornment.get(depth, '')
+        width=len(title)
+        text='''\n{0}\n{1:<{2}}\n{0}\n'''.format(adornment*width, title, width)
         return text
 
     def section_header(self, header, depth = 1):
-        adornment = ''
-        for n in header:
-            adornment += self.adornment.get(depth, '')
-        text='''\n{0}\n{1}\n'''.format(header, adornment)
+        adornment = self.adornment.get(depth, '')
+        width=len(header)
+        text='''\n{0:<{1}}\n{2}\n'''.format(header, width, adornment*width)
         return text
-
-    def simple_table_header(self, header, length):
-        adornment = ''
-        for n in range(0, length):
-            adornment += self.adornment.get(1, '')
-        h = "{:^{width}}".format(header, width=length)
-        text='''\n{0}\n{1}\n{0}\n'''.format(adornment, h)
-        return text
-
-    def simple_table_body(self, body, length):
-        text = ''
-        text += '''{0}\n'''.format(body)
-        return text
-
-    def simple_table_old(self, headers, col_list, col_selector):
-        width0 = max(self.get_row_length(headers, col_list, 0))
-        width1 = max(self.get_row_length(headers, col_list, 1))
-        print(str(width0) + ' ' + str(width1))
-        tbl  = self.simple_table_header(headers[0], width0)
-        for tpl in col_list:
-            tbl += self.simple_table_body(tpl[0], width0)
+    
+    def simple_table(self, header_list, subject_list):
+        tbl = PrettyTable(header_list)
+        for row in subject_list:
+            tbl.add_row(row)
+        tbl.align="l"
+        tbl.hrules=prettytable.ALL
         print(tbl)
-
-
-    def get_row_length(self, headers, col_list, index):
-        width_header  = len(max(headers[index], key=len))
-        width_content =  0
-        for item in col_list:
-            width_content = max(width_content, len(item[index]))
-        return (width_header, width_content)
-
-    def simple_table(self, headers, col_list, col_selector):
-        print(len(headers), len(col_list), len(col_selector))
-        if not max(col_selector) <= len(headers): raise AssertionError
-        if not max(col_selector) <= len(col_list[col_selector[0]]): raise AssertionError
-        index = col_selector[0]
-        width0 = self.get_width(headers, col_list[index], index)
-        print(width0)
-        h = self.simple_table_header(headers[0], width0)
-        print(h)
-
-    def get_width(self, headers, col_list, index):
-        hdr_max = len(headers[index])
-        col_max = 0
-        for n in col_list:
-            #print("width of " + col_list[index])
-            col_max = max(col_max, len(max(col_list[index], key=len)))
-        width = max(hdr_max, col_max)
-        #print(hdr_max, col_max)
-        return width
-
-
-    def get_column_width(self, col_list, idx):
-        col_max = 0;
-        for n in col_list:
-            col_max = max(col_max, len(max(col_list[idx], key=len)))
-        return col_max
 
 
 class TestMatrixGenerator:
     rst = RstMaker()
 
-    def bar(self):
-        for root, dirs, files in sorted(os.walk('test')):
+    def contents(self):
+        for root, dirs, files in sorted(os.walk('test_case')):
             #print("root=" + str(root) + ", dirs=" + str(dirs) + ", files=" + str(files))
             if files and not dirs:
                 subject_list = []
@@ -102,9 +53,8 @@ class TestMatrixGenerator:
 
                 print(self.rst.title_header(subject, 1))
                 headers = ("File", "Input", "Expected")
-                self.rst.simple_table(headers, subject_list, (0,1))
-                for test in subject_list:
-                    print(test)
+                self.rst.simple_table(headers, subject_list)
+
 
     def read_content(self, file):
         fd = open(file, "r")
@@ -113,5 +63,5 @@ class TestMatrixGenerator:
 
 
 if __name__ == "__main__":
-    gen = TestMatrixGenerator()
-    gen.bar()
+    table = TestMatrixGenerator()
+    table.contents()
