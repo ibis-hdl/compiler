833cb8467352b53a92b3c4fd5540ea3fb1583261
Olaf <olaf@home.lan>
2017-04-27 18:39:35 +0200

    fixed indent

--- playground/vhdl93/cxx_indent_ostreambuf.cpp
+++ playground/vhdl93/cxx_indent_ostreambuf.cpp
@@ -33,12 +33,12 @@ class indent_sbuf : public std::streambuf
     std::streambuf*     m_sbuf;
     std::string         m_indent_str;
     bool                m_start_of_line;
-    static const int    TAB_WIDTH = 2;
+    static const int    TAB_WIDTH = 4;
 
 public:
     explicit indent_sbuf(std::streambuf* sbuf, size_t indent = 0)
         : m_sbuf{ sbuf }
-        , m_indent_str(indent, '.')
+        , m_indent_str(indent, ' ')
         , m_start_of_line{ false }
     { }
 
@@ -49,14 +49,14 @@ public:
 
     indent_sbuf& increase()
     {
-        m_indent_str = std::string(m_indent_str.size() + TAB_WIDTH, '.');
+        m_indent_str = std::string(m_indent_str.size() + TAB_WIDTH, ' ');
         return *this;
     }
 
     indent_sbuf& decrease()
     {
         if(m_indent_str.size() > TAB_WIDTH) {
-            m_indent_str = std::string(m_indent_str.size() - TAB_WIDTH, '.');
+            m_indent_str = std::string(m_indent_str.size() - TAB_WIDTH, ' ');
         }
         else {
             m_indent_str.clear();
@@ -67,11 +67,22 @@ public:
 private:
     int_type overflow(int_type chr) override
     {
+#if 1
+        int_type const put_ret{ m_sbuf->sputc(chr) };
+        if(chr == '\n') {
+            /* Note: On success the number of characters written is returned
+             *       and no check is done here. No fault expected ;-) */
+            m_sbuf->sputn( m_indent_str.data(), m_indent_str.size() );
+        }
+        return put_ret;
+#else
+        // this code does wrong indent
         if (m_start_of_line && chr != '\n') {
             m_sbuf->sputn( m_indent_str.data(), m_indent_str.size() );
         }
         m_start_of_line = (chr == '\n');
         return m_sbuf->sputc( chr );
+#endif
     }
 };
 
@@ -129,10 +140,10 @@ struct printer
             if(verbose) {
                 os << increase_indent << "(";
                 os << name;
-                if(name_pfx)
+                if(name_pfx) {
                     os << "<" << name_pfx << ">";
+                }
                 os << "\n";
-                //os << "_"; // FixMe, required for correct indent
             }
         }
 
