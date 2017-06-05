f21cfc1d79b236621beeaf68274c8be6e24dff0e
Olaf <olaf@home.lan>
2017-06-05 13:14:47 +0200

    sequence_of_statement (untested) added

---
+++ sources/vhdl93/include/eda/vhdl93/sequence_of_statements.hpp
@@ -0,0 +1,28 @@
+/*
+ * sequence_of_statements.hpp
+ *
+ *  Created on: 05.06.2017
+ *      Author: olaf
+ */
+
+#ifndef SOURCES_VHDL93_INCLUDE_EDA_VHDL93_SEQUENCE_OF_STATEMENTS_HPP_
+#define SOURCES_VHDL93_INCLUDE_EDA_VHDL93_SEQUENCE_OF_STATEMENTS_HPP_
+
+
+#include <eda/vhdl93/ast/namespace_alias.hpp>
+#include <eda/vhdl93/ast/position_tagged.hpp>
+
+#include <eda/vhdl93/ast/sequential_statement.hpp>
+#include <vector>
+
+
+namespace eda { namespace vhdl93 { namespace ast {
+
+
+struct sequence_of_statements : std::vector<ast::sequential_statement> { };
+
+
+} } } // namespace eda.vhdl93.ast
+
+
+#endif /* SOURCES_VHDL93_INCLUDE_EDA_VHDL93_SEQUENCE_OF_STATEMENTS_HPP_ */
