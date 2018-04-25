e6bad310853e847f1847e7e1f1d08d9b82c26173
Olaf <olaf@home.lan>
2018-04-25 20:33:37 +0200

    using system default g++ compiler

--- eclipse-cmake.sh
+++ eclipse-cmake.sh
@@ -6,7 +6,7 @@ SCRIPT_PATH="${BASH_SOURCE%/*}"
 # this script is in the same directory
 EDA_SRC_DIR=${SCRIPT_PATH}
 
-CMAKE_BIN=/usr/local/gcc-7.1.0/bin/cmake
+CMAKE_BIN=cmake
 CPU_COUNT=$(grep ^processor /proc/cpuinfo | wc -l)
 CPU_COUNT=2 # override
 
@@ -18,7 +18,7 @@ ${CMAKE_BIN} ${EDA_SRC_DIR} \
         -DCMAKE_RULE_MESSAGES:BOOL=OFF \
         -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
         -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_CXX_COMPILER=/usr/local/gcc-7.1.0/bin/g++ \
+        -DCMAKE_CXX_COMPILER=g++ \
         -DCMAKE_INSTALL_PREFIX=stage \
         -DBUILD_SHARED_LIBS:BOOL=OFF 
         
