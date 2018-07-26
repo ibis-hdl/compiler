21e4820e23cea09f7a58d69e9d684851cabd9af3
Olaf <olaf@home.lan>
2018-07-26 16:41:05 +0200

    PlayGround AST tagget stats
    - concept improved

--- playground/cxx/cxx_ast_tagged_stats.cpp
+++ playground/cxx/cxx_ast_tagged_stats.cpp
@@ -4,7 +4,7 @@
  *  Created on: 21.07.2018
  *      Author: olaf
  *
- *  https://wandbox.org/permlink/Wd4Aqz2rBY0qUvXh
+ * https://wandbox.org/permlink/K64boWhNZ4OORUdg
  */
 
 #include <iostream>
@@ -27,130 +27,226 @@ struct baz : position_tagged { };
 
 
 
-struct tag_data
+struct collect_worker
 {
     typedef std::map<std::string_view, std::size_t> map_type;
     typedef map_type::iterator                      iterator_type;
     typedef map_type::value_type                    value_type;
 
-    void tagged(std::string_view const& node_name) {
-        ++tagged_map[node_name];
+    collect_worker(map_type& tagged_map_, map_type& untagged_map_)
+    : tagged_map{ tagged_map_ }
+    , untagged_map{ untagged_map_ }
+    {}
+
+    template<typename NodeT>
+    void operator()(NodeT const&, std::string_view const& node_name) const {
+        if constexpr (std::is_base_of_v<ast::position_tagged, std::decay_t<NodeT>>) {
+            ++tagged_map[node_name];
+        }
+        else {
+            ++untagged_map[node_name];
+        }
     }
 
-    void untagged(std::string_view const& node_name) {
-        ++untagged_map[node_name];
-    }
-
-    // Todo: - tag dispatch {ascending, descending} sorted
-    //       - variadic template to allow sort(tagged, untagged)
-    auto sort(map_type const& map) const {
-#if 0
-        // XXX doesn't compile
-        typedef map_type::value_type pair_type;
-#else
-        // ok, compiles with mutable keys
-        typedef std::pair<map_type::key_type, map_type::mapped_type> pair_type;
-#endif
-        std::vector<pair_type> vec;
-        vec.reserve(map.size());
-
-        std::copy(map.begin(), map.end(),
-                  std::back_inserter<std::vector<pair_type>>(vec));
-
-        std::sort(vec.begin(), vec.end(),
-                  [](pair_type const& lhs, pair_type const& rhs) {
-                        if (lhs.second != rhs.second)
-                            return lhs.second < rhs.second; // sort-by-value
-                        return lhs.first < rhs.first;       // sort-by-key otherwise
-                  });
-        return vec;
-    }
-
-    map_type                                        tagged_map;
-    map_type                                        untagged_map;
+    map_type&                           tagged_map;
+    map_type&                           untagged_map;
 };
 
 
-struct tagged_visitor
+template<typename WorkerT>
+struct visitor_base
 {
-    tagged_visitor(tag_data& data_)
-    : data{ data_ }
+    template <typename... WorkerArgs>
+    visitor_base(WorkerArgs&&... args)
+    : worker{ std::forward<WorkerArgs>(args)... }
     { }
 
-    template<typename NodeT>
-    void count(NodeT const&, std::string_view const& node_name) const {
-        if constexpr (std::is_base_of_v<ast::position_tagged, std::remove_reference_t<NodeT>>) {
-            data.tagged(node_name);
-        }
-        else {
-            data.untagged(node_name);
-        }
-    }
+    visitor_base(visitor_base const&) = delete;
+    visitor_base const& operator=(visitor_base const&) = delete;
 
     void operator()(ast::foo const& node) const {
         static char const symbol[]{ "foo" };
-        count(node, symbol);
+        worker(node, symbol);
     }
     void operator()(ast::bar const& node) const {
         static char const symbol[]{ "bar" };
-        count(node, symbol);
+        worker(node, symbol);
     }
-    void operator()(ast::baz const& node) const  {
-        static char const symbol[]{ "baz" };
-        count(node, symbol);
+    void operator()(ast::baz const& node) const {
+        static char const symbol[]{ "baz-baz" };
+        worker(node, symbol);
     }
 
-    tag_data&                              data;
+    WorkerT const                              worker;
 };
 
-struct tagged_stats
-{
-    tagged_stats(tag_data& data)
-    : data{ data }
-    { }
 
-    void print_map(tag_data::map_type& map) const {
-        auto const sorted = data.sort(map);
-        for (auto const& [node, count] : sorted) {
-            std::cout << node << ": " << count << "\n";
+///////////////////////////////////////////////////////////////////////////////
+template<typename T>
+struct is_vector : std::false_type { };
+
+template<typename T>
+struct is_vector<std::vector<T>> : std::true_type { };
+
+template<typename T>
+inline constexpr bool is_vector_v = is_vector<T>::value;
+///////////////////////////////////////////////////////////////////////////////
+#if 0
+int main()
+{
+    std::vector<int> i_vec;
+    int i;
+    std::cout << std::boolalpha << is_vector_v<decltype(i_vec)> << "\n";
+    std::cout << std::boolalpha << is_vector_v<decltype(i)> << "\n";
+
+}
+#endif
+
+
+template<typename KeyT, typename ValueT>
+struct format
+{
+    format(std::map<KeyT, ValueT> const& map_)
+    : map{ map_ }
+    {}
+
+    std::ostream& print(std::ostream& os) const
+    {
+        std::size_t const max_key_size = [this] {
+
+			std::size_t size{ 0 };
+			std::for_each(map.begin(), map.end(),
+				[&size](auto const& pair) {
+					size = std::max(size, pair.first.size());
+				}
+			);
+			return    size;
+        } ();
+
+        std::string const padding(max_key_size, ' ');
+
+        for (auto const& [key, value] : map) {
+            os << key   << std::string_view(padding).substr(0, max_key_size - key.size())
+               << " : " << value << "\n";
         }
+        return os;
     }
 
-    void tagged() const {
-        std::cout << "Tagged:\n";
-        print_map(data.tagged_map);
-        std::cout << "\n";
-    }
-
-    void untagged() const {
-        std::cout << "UnTagged:\n";
-        print_map(data.untagged_map);
-        std::cout << "\n";
-    }
-
-    tag_data&                              data;
+    std::map<KeyT, ValueT> const&               map;
 };
 
-struct statistic
+template<typename KeyT, typename ValueT>
+std::ostream& operator<<(std::ostream& os, format<KeyT, ValueT> const& printer) {
+    return printer.print(os);
+}
+
+template<typename KeyT, typename ValueT>
+struct format2
 {
-    statistic()
-    : visitor{ map }
-    , stats{ map }
-    { }
+    format2(std::vector<std::pair<KeyT, ValueT>> const& vec_)
+    : vec{ vec_ }
+    {}
 
-    template<typename NodeT>
-    void operator()(NodeT const& node) const {
-        visitor(node);
+    std::ostream& print(std::ostream& os) const
+    {
+        std::size_t const max_key_size = [this] {
+
+			std::size_t size{ 0 };
+			std::for_each(vec.begin(), vec.end(),
+				[&size](auto const& pair) {
+					size = std::max(size, pair.first.size());
+				}
+			);
+			return size;
+        } ();
+
+        std::string const padding(max_key_size, ' ');
+
+        for (auto const& [key, value] : vec) {
+            os << key   << std::string_view(padding).substr(0, max_key_size - key.size())
+               << " : " << value << "\n";
+        }
+        return os;
     }
 
-    void print() const {
-        stats.tagged();
-        stats.untagged();
-    }
+    std::vector<std::pair<KeyT, ValueT>> const&     vec;
+};
 
-    tag_data                                map;
-    tagged_visitor                          visitor;
-    tagged_stats                            stats;
+template<typename KeyT, typename ValueT>
+std::ostream& operator<<(std::ostream& os, format2<KeyT, ValueT> const& printer) {
+    return printer.print(os);
+}
+
+
+template<typename... KeyT, typename... ValueT>
+auto as_vec(std::map<KeyT, ValueT> const&... maps)
+{
+    std::size_t const size = (maps.size() + ...);   // Fold expression
+
+    using key_type = std::tuple_element_t<0, std::tuple<KeyT...>>;
+    using value_type= std::tuple_element_t<0, std::tuple<ValueT...>>;
+
+    typedef std::pair<key_type, value_type> 		pair_type;
+
+	std::vector<pair_type> vec;
+	vec.reserve(size);
+
+    auto const push_back = [&vec](auto const& map) {
+        std::copy(map.begin(), map.end(),
+	        std::back_inserter<std::vector<pair_type>>(vec));
+    };
+
+    (push_back(maps), ...);  // Fold expression
+
+    return vec;
+}
+
+
+struct ascending_tag {};
+struct descending_tag {};
+
+template<typename KeyT, typename ValueT, typename Tag> struct predicate { };
+
+template<typename KeyT, typename ValueT> struct predicate<KeyT, ValueT, ascending_tag>
+{
+    typedef std::pair<KeyT, ValueT>                 pair_type;
+
+    bool operator()(pair_type const& lhs, pair_type const& rhs) const {
+        if (lhs.second != rhs.second)
+            return lhs.second < rhs.second; // sort-by-value
+        return lhs.first < rhs.first;       // sort-by-key otherwise
+    };
+};
+
+template<typename KeyT, typename ValueT> struct predicate<KeyT, ValueT, descending_tag>
+{
+    typedef std::pair<KeyT, ValueT>                 pair_type;
+
+    bool operator()(pair_type const& lhs, pair_type const& rhs) const {
+        if (lhs.second != rhs.second)
+            return lhs.second > rhs.second; // sort-by-value
+        return lhs.first < rhs.first;       // sort-by-key otherwise
+    };
+};
+
+
+
+template<typename Tag>
+struct map_sort
+{
+template<typename... KeyT, typename... ValueT>
+auto operator()(std::map<KeyT, ValueT> const&... maps)
+{
+	auto vec = as_vec(std::map<KeyT, ValueT>(maps)...);
+
+    using key_type = std::tuple_element_t<0, std::tuple<KeyT...>>;
+    using value_type= std::tuple_element_t<0, std::tuple<ValueT...>>;
+	using predicate_ = predicate<key_type, value_type, Tag>;
+
+	std::sort(vec.begin(), vec.end(), predicate_{});
+
+	return vec;
+}
 };
 
 
@@ -160,10 +256,119 @@ int main()
     ast::bar bar;
     ast::baz baz;
 
-    statistic stats;
-    stats(foo);
-    stats(foo);
-    stats(bar);
-    stats(baz);
-    stats.print();
+    collect_worker::map_type tagged_map;
+    collect_worker::map_type untagged_map;
+
+    visitor_base<collect_worker> collect(tagged_map, untagged_map);
+    collect(foo);
+    collect(foo);
+    collect(bar);
+    collect(baz);
+
+    std::cout << format(tagged_map) << "\n";
+    std::cout << format(untagged_map) << "\n";
+
+    auto node_list = as_vec(tagged_map, untagged_map);
+    auto sorted = map_sort<ascending_tag>{}(tagged_map, untagged_map);
+
+    std::cout << "sorted:\n";
+    std::cout << format2(sorted) << "\n";
 }
+
+#if 0
+// *** Map sort ***
+// https://wandbox.org/permlink/3nfdAXnFp4CNjLfL
+
+#include <iostream>
+
+#include <optional>
+#include <string_view>
+#include <map>
+#include <vector>
+#include <algorithm>
+
+struct tag_data
+{
+    typedef std::map<std::string, std::size_t>      map_type;
+    typedef map_type::iterator                      iterator_type;
+    typedef map_type::value_type                    value_type;
+
+    void tagged(std::string const& node_name) {
+        ++tagged_map[node_name];
+    }
+
+    void untagged(std::string const& node_name) {
+        ++untagged_map[node_name];
+    }
+
+    map_type                                        tagged_map;
+    map_type                                        untagged_map;
+};
+
+
+struct ascending_tag {};
+struct descending_tag {};
+
+template<typename KeyT, typename ValueT, typename Tag> struct predicate { };
+
+template<typename KeyT, typename ValueT> struct predicate<KeyT, ValueT, ascending_tag>
+{
+    typedef std::pair<KeyT, ValueT>                 pair_type;
+
+    bool operator()(pair_type const& lhs, pair_type const& rhs) const {
+        if (lhs.second != rhs.second)
+            return lhs.second < rhs.second; // sort-by-value
+        return lhs.first < rhs.first;       // sort-by-key otherwise
+    };
+};
+
+template<typename KeyT, typename ValueT> struct predicate<KeyT, ValueT, descending_tag>
+{
+    typedef std::pair<KeyT, ValueT>                 pair_type;
+
+    bool operator()(pair_type const& lhs, pair_type const& rhs) const {
+        if (lhs.second != rhs.second)
+            return lhs.second > rhs.second; // sort-by-value
+        return lhs.first < rhs.first;       // sort-by-key otherwise
+    };
+};
+
+
+// ToDo:
+// - variadic template of sort() to allow sort(map1,  map2)
+template< typename Tag, typename KeyT, typename ValueT>
+auto map_sort(std::map<KeyT, ValueT> const& map)
+{
+	typedef KeyT                      				key_type;
+	typedef ValueT                     				mapped_type;
+	typedef std::pair<key_type, mapped_type> 		pair_type;
+
+	std::vector<pair_type> vec;
+	vec.reserve(map.size());
+
+	std::copy(map.begin(), map.end(),
+			  std::back_inserter<std::vector<pair_type>>(vec));
+
+	using predicate_ = predicate<key_type, mapped_type, Tag>;
+	std::sort(vec.begin(), vec.end(), predicate_{});
+
+	return vec;
+}
+
+
+
+int main()
+{
+    tag_data data;
+    data.tagged("foo");
+    data.tagged("foo");
+    data.tagged("boo");
+    data.untagged("baz");
+
+    auto const sorted = map_sort<descending_tag>(data.tagged_map);
+
+    for (auto const& [key, value] : sorted) {
+        std::cout << key << " : " << value << "\n";
+    }
+}
+#endif
