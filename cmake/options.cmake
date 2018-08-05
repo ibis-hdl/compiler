e9ad9cb3178b522a7a149fc7e9b77f0a7bf77af1
Olaf <olaf@home.lan>
2018-08-05 13:59:32 +0200

    Started Boost.Stacktrace, but my Fedora hasn't it yet

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -22,7 +22,7 @@ option(EDA_WITH_BOOST_STACKTRACE "Enable support for the Boost::stacktrace libra
 ##
 # Build Option: run EDA under Valgrind.
 option(EDA_RUN_ON_VALGRIND "Configure EDA to be run on Valgrind." OFF)
-mark_as_advanced(PIRANHA_RUN_ON_VALGRIND)
+mark_as_advanced(EDA_RUN_ON_VALGRIND)
 
 
 
@@ -43,4 +43,9 @@ option(EDA_BUILD_TESTS "Build tests" ON)
 # Sanity Checks
 if(EDA_WITH_BOOST_STACKTRACE AND EDA_WITH_GDB_STACKTRACE)
     message(FATAL_ERROR "Only one stacktrace option is useful and hence allowed.")
+endif()
+
+
+if(EDA_WITH_BOOST_STACKTRACE)
+    message(FATAL_ERROR "Sorry, my Fedora box has only boost 1.64; boost.stacktrace is since 1.65 ...")
 endif()
---
+++ sources/ibis/src/stacktrace_boost.cpp
@@ -0,0 +1,14 @@
+/*
+ * stacktrace_boost.cpp
+ *
+ *  Created on: 05.08.2018
+ *      Author: olaf
+ */
+
+#include <boost/stacktrace.hpp>
+
+#include <iostream>
+
+
+
+
