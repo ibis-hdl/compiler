2d0601b46a7ab659d9e008096b81b30c462ea22e
Olaf <olaf@home.lan>
2018-08-08 11:22:05 +0200

    Playground/CXX added basic concept for color on windows

--- playground/cxx/cxx_color_output_win.cpp
+++ playground/cxx/cxx_color_output_win.cpp
@@ -20,42 +20,64 @@
 #include <windows.h>
 
 
+namespace windows {
+
+/* [Using the High-Level Input and Output Functions](
+ * https://docs.microsoft.com/en-us/windows/console/using-the-high-level-input-and-output-functions) */
+enum class attribute : uint16_t {
+    // FixMe: Text attribute - unsupported, how to ???
+    Attributes_Off 		= 0x100,
+    Text_Bold 			= 0x101,
+    Text_Underscore 	= 0x102,
+    Text_Blink 			= 0x103,
+    Text_Reverse 		= 0x104,
+    Text_Concealed 		= 0x105,
+    // Foreground colors
+    Foreground_Black    = 0,
+    Foreground_Red      = FOREGROUND_RED,
+    Foreground_Green    = FOREGROUND_GREEN,
+    Foreground_Yellow   = FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY,
+    Foreground_Blue     = FOREGROUND_BLUE,
+    Foreground_Magenta  = FOREGROUND_BLUE | FOREGROUND_RED,
+    Foreground_Cyan     = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY,
+    Foreground_White    = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED,
+    // Background colors
+    Background_Black    = 0,
+    Background_Red      = BACKGROUND_RED,
+    Background_Green    = BACKGROUND_GREEN,
+    Background_Yellow   = BACKGROUND_GREEN | BACKGROUND_RED | BACKGROUND_INTENSITY,
+    Background_Blue     = BACKGROUND_BLUE,
+    Background_Magenta  = BACKGROUND_BLUE | BACKGROUND_RED,
+    Background_Cyan     = BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_INTENSITY,
+    Background_White    = BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED,
+};
+
+} // namespace windows
+
+
 // https://github.com/Baltasarq/cscrutil/blob/master/src/scrutil.c
-typedef enum _Color {
-    scrBlack, scrBlue, scrRed, scrMagenta,
-    scrGreen, scrCyan, scrYellow, scrWhite,
-    scrUndefinedColor
-} Color;
 
-static short int winInkColors[ scrUndefinedColor + 1];
-static short int winPaperColors[ scrUndefinedColor + 1 ];
-
-static void initWindowsColors()
-{
-	// wincon.h, all defines!
-    winInkColors[ scrBlack ]     = 0;
-    winInkColors[ scrBlue ]      = FOREGROUND_BLUE;
-    winInkColors[ scrRed ]       = FOREGROUND_RED;
-    winInkColors[ scrMagenta ]   = FOREGROUND_BLUE | FOREGROUND_RED;
-    winInkColors[ scrGreen ]     = FOREGROUND_GREEN;
-    winInkColors[ scrCyan ]      = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY;
-    winInkColors[ scrYellow ]    = FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY;
-    winInkColors[ scrWhite ]     = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED;
-    winInkColors[ scrUndefinedColor ]  = 0;
-
-    winPaperColors[ scrBlack ]   = 0;
-    winPaperColors[ scrBlue ]    = BACKGROUND_BLUE;
-    winPaperColors[ scrRed ]     = BACKGROUND_RED;
-    winPaperColors[ scrMagenta ] = BACKGROUND_BLUE | BACKGROUND_RED;
-    winPaperColors[ scrGreen ]   = BACKGROUND_GREEN;
-    winPaperColors[ scrCyan ]    = BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_INTENSITY;
-    winPaperColors[ scrYellow ]  = BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_INTENSITY;
-    winPaperColors[ scrWhite ]   = BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED;
-    winPaperColors[ scrUndefinedColor ] = 0;
-}
 
 int main()
 {
-	initWindowsColors();
+	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
 
+	if(hStdout == INVALID_HANDLE_VALUE) {
+		std::cout << "no valid handle\n";
+	}
+
+	// Remember how things were when we started
+	CONSOLE_SCREEN_BUFFER_INFO csbi{};
+	GetConsoleScreenBufferInfo( hStdout, &csbi );
+
+	using windows::attribute;
+
+	SetConsoleTextAttribute(hStdout,
+	    		(WORD)attribute::Foreground_Red | (WORD)attribute::Background_Yellow);
+
+	std::cout << "Hello World\n";
+
+	SetConsoleTextAttribute( hStdout, csbi.wAttributes );
+
+	std::cout << "Hello\n";
 }
