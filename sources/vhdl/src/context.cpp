5d3195c65f58e066404b5dc69d72bfe0b6725a32
Olaf <olaf@home.lan>
2018-08-22 08:13:14 +0200

    EDA/VHDL context translation simplified
    - error/warning count message simplified

--- sources/vhdl/src/context.cpp
+++ sources/vhdl/src/context.cpp
@@ -22,63 +22,45 @@ context::context()
 { }
 
 
-namespace /* anonymous */ {
-
-// Fix for Clang '-Weverything' Diagnostic at format(translate("...", count)):
-//     warning: implicit conversion loses integer precision: 'const size_t'
-//     (aka 'const unsigned long') to 'int' [-Wshorten-64-to-32]
-//
-// Obviously boost.locale uses an int32 in the [plural form](
-//  https://www.boost.org/doc/libs/1_67_0/libs/locale/doc/html/messages_formatting.html#plural_forms).
-// To avoid further references by '-Weverything' diagnostics a plural form
-// for count is introduced. For plural form the concrete maximum value isn't
-// critical in human language.
-int plural_count(size_t count) {
-    static constexpr int N{ std::numeric_limits<int>::max() };
-    if (count > N) return N;
-    return static_cast<int>(count);
-};
-
-} // anonymous namespace
-
-
 std::ostream& failure_status::operator()(std::ostream& os) const
 {
     using boost::locale::translate;
     using boost::locale::format;
 
-    auto const error_message = [](size_t count) {
-        return util::make_iomanip([count](std::ostream& os_) {
-            if(count) {
-                os_ << format(translate(
-                       "{1} error", "{1} errors", plural_count(count)))
-                       % count
-                       ;
-            }
-        });
+    // Fix for Clang '-Weverything' Diagnostic at format(translate("...", count)):
+    //     warning: implicit conversion loses integer precision: 'const size_t'
+    //     (aka 'const unsigned long') to 'int' [-Wshorten-64-to-32]
+    //
+    // Obviously boost.locale uses an int32 in the [plural form](
+    //  https://www.boost.org/doc/libs/1_67_0/libs/locale/doc/html/messages_formatting.html#plural_forms).
+    // To avoid further references by '-Weverything' diagnostics a plural form
+    // for count is introduced. For plural form the concrete maximum value isn't
+    // critical in human language.
+    auto const plural_count = [](size_t count) {
+        static constexpr int N{ std::numeric_limits<int>::max() };
+        if (count > N) return N;
+        return static_cast<int>(count);
     };
 
-    auto const warning_message = [](size_t count) {
-        return util::make_iomanip([count](std::ostream& os_) {
-            if(count) {
-                os_ << format(translate(
-                       "{1} warning", "{1} warnings", plural_count(count)))
-                       % count
-                       ;
-            }
-        });
-    };
 
-    bool const any_failure = ctx.error_count || ctx.warning_count;
-    bool const all_failure = ctx.error_count && ctx.warning_count;
+    // concept, see [Wanbox](https://wandbox.org/permlink/VJrqXuEFppw1htY7)
+    auto const error_message = (format(translate("{1} error", "{1} errors",
+        plural_count(ctx.error_count))) % ctx.error_count).str();
 
-    // FixMe: complicated for the translator, see https://wandbox.org/permlink/lgU6DK75axnthjWz
-    if(any_failure) {
-        os << error_message(ctx.error_count)
-           << (all_failure ? translate(" and ") : translate("")) // same type req.!
-           << warning_message(ctx.warning_count)
-           << translate(" generated.")
-           ;
+    auto const warning_message = (format(translate("{1} warning", "{1} warnings",
+        plural_count(ctx.warning_count))) % ctx.warning_count).str();
+
+    if (ctx.error_count && ctx.warning_count) {
+        os << format(translate("{1} and {2} generated.")) % error_message % warning_message;
+    }
+    else if (ctx.error_count) {
+        os << format(translate("{1} generated.")) % error_message;
+    }
+    else if (ctx.warning_count) {
+        os << format(translate("{1} generated.")) % warning_message;
+    }
+    else {
+        // all went fine
     }
 
     return os;
@@ -87,72 +69,3 @@ std::ostream& failure_status::operator()(std::ostream& os) const
 
 } } // namespace eda.vhdl
 
-
-
-#if 0
-namespace detail {
-
-struct error_message {
-    std::size_t const count;
-    explicit error_message(std::size_t count_) : count{ count_ } { }
-    std::ostream& operator()(std::ostream& os) const {
-        using boost::locale::format;
-        using boost::locale::translate;
-        if(count) {
-            os << format(translate(
-                  "{1} error", "{1} errors", count))
-                  % count
-                  ;
-        }
-        return os;
-    }
-};
-
-std::ostream& operator<<(std::ostream& os, error_message const& err_msg) {
-    return err_msg(os);
-}
-
-struct warning_message {
-    std::size_t const count;
-    explicit warning_message(std::size_t count_) : count{ count_ } { }
-    std::ostream& operator()(std::ostream& os) const {
-        using boost::locale::format;
-        using boost::locale::translate;
-        if(count) {
-            os << format(translate(
-                  "{1} warning", "{1} warnings", count))
-                  % count
-                  ;
-        }
-        return os;
-    }
-};
-
-std::ostream& operator<<(std::ostream& os, warning_message const& warn_msg) {
-    return warn_msg(os);
-}
-
-} // namespace detail
-
-
-std::ostream& failure_status::operator()(std::ostream& os) const
-{
-    using detail::warning_message;
-    using detail::error_message;
-    using boost::locale::translate;
-
-    bool const any_failure = ctx.error_count || ctx.warning_count;
-
-    // FixMe: complicated for the translator, see https://wandbox.org/permlink/lgU6DK75axnthjWz
-    if(any_failure) {
-        os << error_message(ctx.error_count)
-           << (ctx.warning_count ? translate(" and ") : translate("")) // same type req.!
-           << warning_message(ctx.warning_count)
-           << translate(" generated.")
-           ;
-    }
-
-    return os;
-}
-
-#endif
