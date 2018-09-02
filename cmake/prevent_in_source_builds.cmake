d1bda20991b427ef80a05288112707f9c7a436cb
Olaf <olaf@home.lan>
2018-09-02 11:19:24 +0200

    CMake Build prevent in-source-build

---
+++ cmake/prevent_in_source_builds.cmake
@@ -0,0 +1,17 @@
+#
+# This function will prevent in-source builds
+function(prevent_in_source_builds)
+  # make sure the user doesn't play dirty with symlinks
+  get_filename_component(srcdir "${CMAKE_SOURCE_DIR}" REALPATH)
+  get_filename_component(bindir "${CMAKE_BINARY_DIR}" REALPATH)
+
+  # disallow in-source builds
+  if("${srcdir}" STREQUAL "${bindir}")
+    message("######################################################")
+    message("# You should not be configured & built in the source directory")
+    message("######################################################")
+    message(FATAL_ERROR "Quitting configuration")
+  endif()
+endfunction()
+
+prevent_in_source_builds()
