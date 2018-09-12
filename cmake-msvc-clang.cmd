7c4f5f5f2208fe614e44a36f52f5088256335dbd
Olaf <olaf@home.lan>
2018-09-12 15:46:19 +0200

    Build with Clang/MSVC fix

--- cmake-msvc-clang.cmd
+++ cmake-msvc-clang.cmd
@@ -8,7 +8,7 @@ rem [How to get the path of the batch script in Windows?](
 rem  https://stackoverflow.com/questions/3827567/how-to-get-the-path-of-the-batch-script-in-windows?answertab=active#tab-top)
 set SCRIPT_PATH=%~dp0
 set EDA_SOURCE_DIR=%SCRIPT_PATH%
-set EDA_BUILD_DIR=%EDA_SOURCE_DIR%..\build
+set EDA_BUILD_DIR=%EDA_SOURCE_DIR%..\msvc
 
 rem [Integrate LLVM Clang 4.x.x / 5.x.x / 6.x.x into Visual Studio 2017]
 rem https://stackoverflow.com/questions/43464856/integrate-llvm-clang-4-x-x-5-x-x-6-x-x-into-visual-studio-2017)
@@ -33,15 +33,12 @@ cd %EDA_BUILD_DIR%
 
 cmake %EDA_SOURCE_DIR% ^
     -G %CMAKE_GENERATOR% -T%CMAKE_TOOLSET% ^
-	-DBOOST_ROOT=c:/Boost ^
-	-DBoost_COMPILER=-clang60 ^
-	-DBoost_USE_STATIC_LIBS=TRUE ^
+    -DBOOST_ROOT=c:/Boost ^
+    -DBoost_COMPILER=-clang60 ^
+    -DBoost_USE_STATIC_LIBS=TRUE ^
     -DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE% ^
-    -DCMAKE_RULE_MESSAGES:BOOL=ON ^
-    -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
     -DCMAKE_INSTALL_PREFIX=stage ^
-    -DBUILD_SHARED_LIBS:BOOL=OFF ^
-    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+    -DBUILD_SHARED_LIBS:BOOL=OFF
         
 cmake --build %EDA_BUILD_DIR% --target all --config %CMAKE_BUILD_TYPE%
 
