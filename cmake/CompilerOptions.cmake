db7ff92a12be651fb54da7119da0525d2ba1a613
Olaf <olaf@home.lan>
2018-09-13 20:37:50 +0200

    CMake Build
    - added message about default options, changed warning Level on MSVC
    - added Satanizer on Linux

---
+++ cmake/CompilerOptions.cmake
@@ -0,0 +1,19 @@
+# [How to set compiler options with CMake in Visual Studio 2017](
+# https://stackoverflow.com/questions/45995784/how-to-set-compiler-options-with-cmake-in-visual-studio-2017)
+
+# log all *_INIT variables
+message(STATUS "---------- CMAKE default settings ----------")
+get_cmake_property(_varNames VARIABLES)
+list (REMOVE_DUPLICATES _varNames)
+list (SORT _varNames)
+foreach (_varName ${_varNames})
+    if (_varName MATCHES "_INIT$")
+        message(STATUS "${_varName}=${${_varName}}")
+    endif()
+endforeach()
+message(STATUS "------------------- end --------------------")
+
+if (MSVC)
+    # remove default warning level from CMAKE_CXX_FLAGS_INIT
+    string (REGEX REPLACE "/W[0-4]" "" CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT}")
+endif()
