a84c4f203471b37d1801f743562c80bb2e9952f2
Olaf <olaf@home.lan>
2018-09-13 08:38:30 +0200

    EDA/Common infix_ostream_iter requirements check
    - added 'Concept' check
    - comment about C++17 added

--- sources/common/include/eda/util/infix_ostream_iterator.hpp
+++ sources/common/include/eda/util/infix_ostream_iterator.hpp
@@ -9,6 +9,7 @@
 #define SOURCES_COMMON_INCLUDE_EDA_UTIL_INFIX_OSTREAM_ITERATOR_HPP_
 
 #include <iterator>
+#include <type_traits>
 #include <ostream>
 
 namespace eda {
@@ -24,7 +25,7 @@ namespace util {
  * Usage:
  * \code
  * std::vector<int> numbers = {1, 2, 3, 4};
- * std::copy(begin(numbers), end(numbers),
+ * std::copy(numbers.begin(), numbers.end(),
  *           infix_ostream_iterator<int>(std::cout, ", "));
  * \endcode
  *
@@ -35,6 +36,7 @@ namespace util {
  *
  * \note This may come obsolete with [std::experimental::ostream_joiner](
  *       https://en.cppreference.com/w/cpp/experimental/ostream_joiner)
+ * \note std::iterator is deprecated with C++17, hence isn't derived from this.
  */
 template <class T, class CharT = char, class TraitsT = std::char_traits<CharT>>
 class infix_ostream_iterator
@@ -53,6 +55,10 @@ public:
         : os{ &os_ }
         , actual_delimiter{ nullptr }
     {
+        // Check iterator requirements; especially MSVC copy requires CopyConstructible
+        // ToDo: Concept Check
+        static_assert(std::is_copy_constructible_v<T>, "Iterator must satisfies CopyConstructible");
+        static_assert(std::is_copy_assignable_v<T>,    "Iterator must satisfies CopyAssignable");
     }
 
     infix_ostream_iterator(ostream_type& os_, CharT const* delimiter_)
