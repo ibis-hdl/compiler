973697c21da8deeee07eb1da7a0cd1beb1469446
Olaf <olaf@home.lan>
2021-03-08 20:51:19 +0100

    Doxygen Fixes, works as expected
    - Doc fixes in source
    - doxygen input filter works as expected so far.

--- doc/doxyfilter.py
+++ doc/doxyfilter.py
@@ -2,27 +2,25 @@
 
 import sys, re
 
-def copy():
-    for line in sys.stdin:
+def copy(file):
+    for line in file:
         print(line.rstrip())
 
 def isComment(line):
     comment_re = r'.*(//.*?|/\*.*)'
     return re.match(comment_re, line, flags=re.S)
 
-def filter():
+def filter(file):
     fixme_re = r'(FIXME)(:)?'
-    # [How do you read from stdin?](
-    #  https://stackoverflow.com/questions/1450393/how-do-you-read-from-stdin)
-    for line in sys.stdin:
+    for line in file:
         if isComment(line):
             #print('# ' + line)
-            x = re.sub(fixme_re, r'\\TODO FixMe', line, flags=re.IGNORECASE)
+            x = re.sub(fixme_re, r'@todo (\\b FixMe)', line, flags=re.IGNORECASE)
             print(x.rstrip())
         else:
             print(line.rstrip())
 
-
 if __name__ == "__main__":
-    #filter()
-    copy()
+    with open(sys.argv[1], 'r') as file:
+        filter(file)
+        #copy(file)
