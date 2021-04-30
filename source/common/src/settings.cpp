035f24f81ab9357b9667807aeac829dffbddb2a4
Olaf <olaf@home.lan>
2021-04-30 16:12:12 +0200

    Fixes testsuite and settings Clang-Win
    - settings compiles on Boost 1.73 using Clang-Win
    - testsuite's rules needs all instances

--- source/common/src/settings.cpp
+++ source/common/src/settings.cpp
@@ -1,8 +1,8 @@
 #include <ibis/settings.hpp>
 #include <ibis/namespace_alias.hpp>
 
-#include <boost/iostreams/device/array.hpp>
 #include <boost/iostreams/stream.hpp>
+#include <boost/iostreams/device/array.hpp>
 
 #include <boost/property_tree/json_parser.hpp>
 
@@ -20,15 +20,21 @@ void settings::insert_json(std::string_view json_sv)
     // https://stackoverflow.com/questions/58524805/is-there-a-way-to-create-a-stringstream-from-a-string-view-without-copying-data)
     using char_type = std::string_view::value_type;
 
+    // FixMe [Future Boost]: std::string_view doesn't seems to be well supported using Boost+Clang-Win:
+    // error: no matching constructor for initialization of
+    // 'boost::iostreams::basic_array_source<char_type>' (aka 'basic_array_source<char>')
+    // note: candidate constructor not viable: no known conversion from
+    // 'std::basic_string_view<char, std::char_traits<char>>::const_iterator' (aka '_String_view_iterator<std::char_traits<char>>')
+    // to 'boost::iostreams::basic_array_source<char>::char_type *' (aka 'char *') for 1st argument
     boost::iostreams::stream<  // --
         boost::iostreams::basic_array_source<char_type> >
-        is(json_sv.begin(), json_sv.size());
+        json_stream(json_sv.data(), json_sv.size());
 
-    pt::ptree other;
-    pt::read_json(is, other);
+    pt::ptree json_pt;
+    pt::read_json(json_stream, json_pt);
 
-    // call ptree's API, if called multiple times, other is appended multiple times too!
-    instance().insert(instance().end(), other.begin(), other.end());
+    // call ptree's API, if called multiple times, it is appended multiple times too!
+    instance().insert(instance().end(), json_pt.begin(), json_pt.end());
 }
 
 }  // namespace ibis
