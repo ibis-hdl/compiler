03a4929f9af9bc2804fc45e97575d416baa16f3a
Olaf <olaf@home.lan>
2022-05-12 17:31:41 +0200

    VS Code DevContainer update
    - update devcontainer to ubuntu 22.04 with clang-14 and gcc-11
    - fix conan cppstd=20 in postCreateCommand.sh
    - added gettext for Linux l10n testing
    - dict update

--- .devcontainer/scripts/postCreateCommand.sh
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -3,8 +3,10 @@
 set -ex
 
 pip3 --disable-pip-version-check --no-cache-dir install conan
-# Conan detects a GCC version > 5 but has adjusted the 'compiler.libcxx' setting to
-# 'libstdc++' for backwards compatibility. Our compiler is likely using the new 
-# CXX11 ABI by default (libstdc++11).
 conan profile new --detect --force default > /dev/null
+# Conan detects a GCC version > 5 but has adjusted the 'compiler.libcxx' setting to
+# 'libstdc++' for backwards compatibility. Our compiler is using the new CXX11 ABI 
+# by default (libstdc++11).
 conan profile update settings.compiler.libcxx=libstdc++11 default
+# C++20 standard
+conan profile update settings.compiler.std=20 default
