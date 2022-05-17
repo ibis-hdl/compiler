021a71f258c35b943642586e0403133bc5e2b280
Olaf <olaf@home.lan>
2022-05-17 20:13:03 +0200

    devcontainer using volumes for conan and ccache
    - using docker volumes

--- .devcontainer/scripts/postCreateCommand.sh
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -1,11 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -ex
+
+# using docker volumes to store data, fix permissions
+# ToDo: solve this problem with correct configurations using e.g. by JSON
+if [ ! -d "/home/vscode/.conan" ]; then
+    sudo mkdir /home/vscode/.conan && chown -R vscode.vscode /home/vscode/.conan
+elif [ $(stat --format '%U' "/home/vscode/.conan") = "root" ]; then
+    echo "fix permissions for conan"
+    sudo chown -R vscode.vscode /home/vscode/.conan
+fi
+
+if [ ! -d "/home/vscode/.cache" ]; then
+    sudo mkdir /home/vscode/.cache && chown -R vscode.vscode /home/vscode/.cache
+elif [ $(stat --format '%U' "/home/vscode/.cache") = "root" ]; then
+    echo "fix permissions for ccache"
+    sudo chown -R vscode.vscode /home/vscode/.cache
+fi
 
 pip3 --disable-pip-version-check --no-cache-dir install conan
 conan profile new --detect --force default > /dev/null
 # Conan detects a GCC version > 5 but has adjusted the 'compiler.libcxx' setting to
-# 'libstdc++' for backwards compatibility. Our compiler is using the new CXX11 ABI 
+# 'libstdc++' for backwards compatibility. Our compiler is using the new CXX11 ABI
 # by default (libstdc++11).
 conan profile update settings.compiler.libcxx=libstdc++11 default
 # C++20 standard
