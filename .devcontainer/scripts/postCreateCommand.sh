1260b8bff505d7805a2d7e96c8230cd5f80811f1
Olaf <olaf@home.lan>
2022-05-27 16:03:36 +0200

    Minor fixes and updates to conan, VS Code, cspell
    - Fix conan's settings.compiler.cppstd=20
    - update cspell config
    - update VS Code settings

--- .devcontainer/scripts/postCreateCommand.sh
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -18,11 +18,11 @@ elif [ $(stat --format '%U' "/home/vscode/.cache") = "root" ]; then
     sudo chown -R vscode.vscode /home/vscode/.cache
 fi
 
-pip3 --disable-pip-version-check --no-cache-dir install conan
+pip3 --disable-pip-version-check --no-cache-dir install wheel conan
 conan profile new --detect --force default > /dev/null
 # Conan detects a GCC version > 5 but has adjusted the 'compiler.libcxx' setting to
 # 'libstdc++' for backwards compatibility. Our compiler is using the new CXX11 ABI
 # by default (libstdc++11).
 conan profile update settings.compiler.libcxx=libstdc++11 default
 # C++20 standard
-conan profile update settings.compiler.std=20 default
+conan profile update settings.compiler.cppstd=20 default
