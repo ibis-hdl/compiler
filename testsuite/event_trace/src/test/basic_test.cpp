c9fd7513438eb9049f8824df48c17eac647171b4
Olaf <olaf@home.lan>
2022-06-30 14:49:10 +0200

    Fix GPL3 and Clang-format
    - fix for GPL3-or-later
    - apply Clang-format

--- testsuite/event_trace/src/test/basic_test.cpp
+++ testsuite/event_trace/src/test/basic_test.cpp
@@ -1,8 +1,7 @@
 //
 // Copyright (c) 2017-2022 Olaf (<ibis-hdl@users.noreply.github.com>).
-// SPDX-License-Identifier: GPL-3.0-only
+// SPDX-License-Identifier: GPL-3.0-or-later
 //
-
 
 #include <ibis/event_trace/event_trace.hpp>
 #include <ibis/event_trace/category.hpp>
