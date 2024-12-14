1557881dfa2eb30d7cd3134327d2d3e7c87994d2
ibis-hdl <83083329+ibis-hdl@users.noreply.github.com>
2024-12-14 14:42:29 +0100

    update CMake builds #27

--- conan_install.py
+++ conan_install.py
@@ -1,9 +1,8 @@
-#! /usr/bin/env python3
-
 import subprocess
 import platform
 import argparse
 from pathlib import Path
+from multipledispatch import dispatch
 
 # Neither 'multipledispatch' nor 'signature_dispatch' work on Github's Windows 2022 action runner, 
 # always get the error e.g.: ModuleNotFoundError: No module named 'signature_dispatch'
@@ -12,41 +11,36 @@ from pathlib import Path
 class ConanInstaller:
     def __init__(self):
         python_ver=tuple(map(int, platform.python_version_tuple()))
-        # match statements are a feature of Python 3.10
+        # match statements are a feature of Python 3.10; take on
+        # [[ENH] PEP 634 - Structural Pattern Matching #4029](
+        # https://github.com/cython/cython/issues/4029)
         assert python_ver > (3,10,0), f"Python > 3.10 required, got {platform.python_version()}"
 
-        # run into [[ENH] PEP 634 - Structural Pattern Matching #4029](
-        # https://github.com/cython/cython/issues/4029) even using Python > 3.10 on GH Windows 2022
-        # image which uses Cython 3.12, so this won't work:
-        """"
         match platform.system():
             case 'Linux':
-                ...
-        """
-        if (platform.system() == 'Linux'):
-            self.python = 'python3'
-            self.shell = ['bash', '-c']
-            self.generator = '"Ninja Multi-Config"'
-            self.valid_profiles = ['gcc', 'clang', 'clang-libc++']
-        elif (platform.system() == 'Windows'):
-            self.python = 'py'
-            self.shell = ['pwsh', '-Command']
-            self.generator = '"Ninja Multi-Config"'
-            self.valid_profiles = ['msvc', 'msvc-cl']            
-        elif (platform.system() == 'Darwin'):
-            self.python = 'python3'
-            self.shell = ['bash', '-c']
-            self.generator = '"Ninja Multi-Config"'
-            self.valid_profiles = ['clang', 'gcc']
-        elif (platform.system() == 'FreeBSD'):
-            self.python = 'python3'
-            self.shell = ['bash', '-c']
-            self.generator = '"Ninja Multi-Config"'
-            self.valid_profiles = ['clang']  
-        else:
-            # https://docs.python.org/3/library/platform.html#platform.system
-            raise Exception(f"Unsupported Platform {platform.system()}")
-                
+                self.python = 'python3'
+                self.shell = ['bash', '-c']
+                self.generator = '"Ninja Multi-Config"'
+                self.valid_profiles = ['gcc', 'clang', 'clang-libc++']
+            case 'Windows':
+                self.python = 'py'
+                self.shell = ['pwsh', '-Command']
+                self.generator = '"Ninja Multi-Config"'
+                self.valid_profiles = ['msvc', 'msvc-cl']
+            case 'Darwin':
+                self.python = 'python3'
+                self.shell = ['bash', '-c']
+                self.generator = '"Ninja Multi-Config"'
+                self.valid_profiles = ['clang', 'gcc']
+            case 'FreeBSD':
+                self.python = 'python3'
+                self.shell = ['bash', '-c']
+                self.generator = '"Ninja Multi-Config"'
+                self.valid_profiles = ['clang']  
+            case _:
+                # https://docs.python.org/3/library/platform.html#platform.system
+                raise Exception(f"Unsupported Platform {platform.system()}")
+ 
         # Set C++ standard to C++20, to avoid warning about modified CMAKE_CXX_STANDARD
         # value defined in conan_toolchain.cmake by ${workspaces}/compiler/CMakeLists.txt
         self.cppstd = 20
@@ -80,8 +74,8 @@ class ConanInstaller:
             print(f"remove former generated Conan CMake preset '{file_name}'")
             Path(file_name).unlink(missing_ok=True)
 
-    #@signature_dispatch
-    def install3(self, build_type: str, conan_profile: str) -> None:
+    @dispatch(str, str)
+    def install(self, build_type: str, conan_profile: str) -> None:
 
         if not build_type.lower() in self.all_build_types:
             raise Exception(f"Unsupported CMAKE_BUILD_TYPE '{build_type}'")
@@ -108,14 +102,14 @@ class ConanInstaller:
                 f"Returned {e.returncode}\n{e}"
             )
 
-    #@signature_dispatch
-    def install2(self, build_types: list, conan_profile: str) -> None:
+    @dispatch(list, str)
+    def install(self, build_types: list, conan_profile: str) -> None:
         for build_type in build_types:
-            self.install3(build_type, conan_profile)
+            self.install(build_type, conan_profile)
 
-    #@signature_dispatch
+    @dispatch(list)
     def install(self, build_types: list) -> None:
-        self.install2(build_types, self.conan_profile)
+        self.install(build_types, self.conan_profile)
 
 if __name__ == '__main__':
     conan = ConanInstaller()
